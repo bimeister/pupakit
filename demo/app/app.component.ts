9350b6bad9b2749b6e45d849d68ab37e56950192
ScarletFlash <scarletflash.dev@gmail.com>
2019-07-15 14:05:39 +0300

    add initial input fields

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -6,8 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   public title: string = 'Pupa kit';
+  public sampleFormControl: FormControl = new FormControl('formControl');
+  private readonly subscription: Subscription = new Subscription();
+
+  public ngOnInit(): void {
+    // tslint:disable-next-line: no-console  no-unbound-method
+    this.subscription.add(this.sampleFormControl.valueChanges.subscribe(console.log));
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   public log(...data: unknown[]): void {
     // tslint:disable-next-line: no-console
---
+++ src/lib/core/components/input/input.component.ts
@@ -0,0 +1,84 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { isNullOrUndefined } from './../../../helpers/is-null-or-undefined.helper';
+
+export type InputSize = 'medium' | 'small';
+export type InputValid = true | false | null;
+@Component({
+  selector: 'pupa-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() public size: InputSize = 'medium';
+  @Input() public valid: InputValid = null;
+  @Input() public disabled: boolean = false;
+  @Input() public placeholder: string = '';
+  @Input()
+  public get value(): string {
+    return this.valueData;
+  }
+  public set value(newValue: string) {
+    this.updateValue(newValue);
+  }
+
+  @Output() public valueChange: EventEmitter<string> = new EventEmitter<string>();
+
+  private valueData: string = '';
+
+  public registerOnChange(fn: VoidFunction): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: VoidFunction): void {
+    this.onTouched = fn;
+  }
+
+  public writeValue(outerValue: unknown): void {
+    this.valueData = String(outerValue);
+  }
+
+  public updateValue(innerValue: string): void {
+    this.valueData = innerValue;
+    this.onChange(innerValue);
+    this.onTouched();
+    this.valueChange.emit(this.value);
+  }
+
+  public getResultClassList(prefix: string): string[] {
+    return [this.size, this.getValidationStateClass()]
+      .filter((innerClassName: string) => !isNullOrUndefined(innerClassName))
+      .map((innerProperty: string) => `${prefix}${innerProperty}`);
+  }
+
+  private getValidationStateClass(): string {
+    switch (this.valid) {
+      case true: {
+        return 'valid';
+      }
+      case false: {
+        return 'invalid';
+      }
+      default:
+        return null;
+    }
+  }
+
+  public onChange: CallableFunction = (innerValue: string) => {
+    innerValue;
+    return;
+  };
+
+  public onTouched: VoidFunction = () => {
+    return;
+  };
+}
