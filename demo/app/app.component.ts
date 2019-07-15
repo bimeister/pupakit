2e8faa5f97b16fa5ddebda9da5bccc552ba46214
Fedor Usakov <fedor.usakov@meistersoft.ru>
2019-07-15 16:21:27 +0300

    switch to rems

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import combos from 'combos';
 import { Subscription } from 'rxjs';
 
@@ -11,26 +11,25 @@ interface ComponentPropertyValues {
   // tslint:disable-next-line: component-selector
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit, OnDestroy {
   public title: string = 'Pupa kit';
-  public sampleFormControl: FormControl = new FormControl('formControl');
+  public sampleFormControl: FormControl = new FormControl('formControl', Validators.required);
 
-  public buttonPropertyValues: ComponentPropertyValues = {
+  public buttonCombos: any[] = combos({
     type: ['solid', 'outlined', 'link'],
     color: ['normal', 'negative', 'positive', 'alert'],
     size: ['large', 'medium', 'small'],
     disabled: [false, true]
-  };
+  });
 
-  public inputPropertyValues: ComponentPropertyValues = {
+  public inputCombos: any[] = combos({
     size: ['medium', 'small'],
-    valid: [true, false, null],
     placeholder: ['placeholder'],
-    disabled: [false, true],
-    value: ['']
-  };
+    disabled: [false, true]
+  });
 
   private readonly subscription: Subscription = new Subscription();
 
