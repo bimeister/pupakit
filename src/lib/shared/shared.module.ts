6d9c7eaaedd4735e50d84d2c15ad90b9ddd63d3f
ScarletFlash <scarletflash.dev@gmail.com>
2019-07-17 14:19:15 +0300

    remove BrowserModule

--- src/lib/shared/shared.module.ts
+++ src/lib/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { IsNullOrUndefinedPipe } from './pipes/is-null-or-undefined.pipe';
 
 const SHARED_COMPONENTS: any[] = [];
 const SHARED_PIPES: any[] = [IsNullOrUndefinedPipe];
-const SHARED_MODULES: any[] = [CommonModule, BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule];
+const SHARED_MODULES: any[] = [CommonModule, FormsModule, ReactiveFormsModule];
 
 @NgModule({
   declarations: [...SHARED_COMPONENTS, ...SHARED_PIPES],
