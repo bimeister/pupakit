e685de607f6f13e4a70317925fc2381c451974f0
Fedor Usakov <fedor.usakov@meistersoft.ru>
2019-07-17 16:06:34 +0300

    remove unused layout

--- src/lib/core/components/layout/layout.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pupa-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
-})
-export class LayoutComponent {}
