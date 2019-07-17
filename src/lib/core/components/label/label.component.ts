267fd4902c8182d039ca94d6e990e14fa01c400d
ScarletFlash <scarletflash.dev@gmail.com>
2019-07-17 16:12:44 +0300

    add label change detection strategy

--- src/lib/core/components/label/label.component.ts
+++ src/lib/core/components/label/label.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'pupa-label',
   templateUrl: './label.component.html',
-  styleUrls: ['./label.component.scss']
+  styleUrls: ['./label.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LabelComponent {}
