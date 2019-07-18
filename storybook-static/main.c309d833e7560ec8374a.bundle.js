3c419b06154daffc04eb0a9e77d11b0f66539bb1
aleksey.usachev <aleksey.usachev@meistersoft.ru>
2019-07-18 13:08:32 +0300

    new font files

---
+++ storybook-static/main.c309d833e7560ec8374a.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(336),p(439),n.exports=p(729)},439:function(n,o,p){"use strict";p.r(o);p(440),p(583),p(586)}},[[0,1,2]]]);
