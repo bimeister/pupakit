64cb96b19d4f3e041eb451468b726cff9b050bf8
Fedor Usakov <fedor.usakov@meistersoft.ru>
2019-07-19 20:55:37 +0300

    Revert "add loader"
    This reverts commit a250ff5d66709fe18d1b00ac86e97144bf3ef865.

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { PupakitCore } from './../../dist/@meistersoft/pupakit';
 import { AppComponent } from './app.component';
@@ -9,7 +8,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, PupakitCore, FormsModule, ReactiveFormsModule, BrowserAnimationsModule],
+  imports: [BrowserModule, PupakitCore, FormsModule, ReactiveFormsModule],
   providers: []
 })
 export class AppModule {}
