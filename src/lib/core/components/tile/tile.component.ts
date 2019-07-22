7707ca9b177602057c0ad1c9abd98412ce688853
Fedor Usakov <fedor.usakov@meistersoft.ru>
2019-07-22 16:48:43 +0300

    review fixes

--- src/lib/core/components/tile/tile.component.ts
+++ src/lib/core/components/tile/tile.component.ts
@@ -1,15 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-export type TileColumnsCount = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '';
 @Component({
   selector: 'pupa-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TileComponent {
-  @Input() public columns: TileColumnsCount = '';
-  public get classModifier(): string {
-    return `tile_columns-${this.columns}`;
-  }
-}
+export class TileComponent {}
