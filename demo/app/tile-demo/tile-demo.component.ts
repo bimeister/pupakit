ee359585253c8f7523df2246facd454e7e8e6ff1
ScarletFlash <scarletflash.dev@gmail.com>
2019-07-22 16:48:43 +0300

    review fixes

--- demo/app/tile-demo/tile-demo.component.ts
+++ demo/app/tile-demo/tile-demo.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import combos from 'combos';
 
 @Component({
   selector: 'demo-loader',
@@ -7,8 +6,4 @@ import combos from 'combos';
   templateUrl: './tile-demo.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TileDemoComponent {
-  public readonly combos: any[] = combos({
-    columns: ['1', '11', '12', '10', '2', '3', '9', '8', '4', '5', '7', '6']
-  });
-}
+export class TileDemoComponent {}
