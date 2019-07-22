005a3ff88f8b8a9d5f3cd8e80c4fabfc49d9a7c5
Fedor Usakov <fedor.usakov@meistersoft.ru>
2019-07-22 13:31:25 +0300

    add tile variations

--- demo/app/tile-demo/tile-demo.component.ts
+++ demo/app/tile-demo/tile-demo.component.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import combos from 'combos';
 
 @Component({
   selector: 'demo-loader',
-  styleUrls: ['../demo.scss'],
+  styleUrls: ['./tile-demo.component.scss'],
   templateUrl: './tile-demo.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TileDemoComponent {}
+export class TileDemoComponent {
+  public readonly combos: any[] = combos({
+    columns: ['1', '11', '12', '10', '2', '3', '9', '8', '4', '5', '7', '6']
+  });
+}
--- src/lib/core/components/tile/tile.component.ts
+++ src/lib/core/components/tile/tile.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
+export type TileColumnsCount = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '';
 @Component({
   selector: 'pupa-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TileComponent {}
+export class TileComponent {
+  @Input() public columns: TileColumnsCount = '';
+  public get classModifier(): string {
+    return `tile_columns-${this.columns}`;
+  }
+}
