2a2f4865259594268ec0666d1a8b71a37f206c49
ScarletFlash <scarletflash.dev@gmail.com>
2019-07-25 12:30:27 +0300

    remove spec files

--- src/lib/shared/pipes/is-null-or-undefined.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsNullOrUndefinedPipe } from './is-null-or-undefined.pipe';
-
-describe('IsNullOrUndefinedPipe', () => {
-  it('create an instance', () => {
-    const pipe: IsNullOrUndefinedPipe = new IsNullOrUndefinedPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
