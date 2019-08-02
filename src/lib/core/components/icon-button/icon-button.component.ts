7ecbc26ad18b6b0924d7f9c6db3ea69b70c3cc92
aleksey.usachev <aleksey.usachev@meistersoft.ru>
2019-08-02 15:54:53 +0300

    add type to icon button

--- src/lib/core/components/icon-button/icon-button.component.ts
+++ src/lib/core/components/icon-button/icon-button.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 import { isNullOrUndefined } from './../../../helpers/is-null-or-undefined.helper';
+import { ButtonType } from '../button/button.component';
 
 export type InputButtonColor = 'dark' | 'light';
 export type InputButtonSize = 'small' | 'medium' | 'large';
@@ -19,6 +20,7 @@ export class IconButtonComponent {
   @Input() public size: InputButtonSize = 'small';
   @Input() public isFloat: boolean = false;
   @Input() public fixed: boolean = false;
+  @Input() public type: ButtonType = 'submit';
 
   @Output() public onclick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
