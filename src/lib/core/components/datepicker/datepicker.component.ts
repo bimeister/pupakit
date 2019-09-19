b143c7193acbd3d82697474f71f29b4ab9aa96ce
ScarletFlash <scarletflash.dev@gmail.com>
2019-09-19 15:31:55 +0300

    add date selector

--- src/lib/core/components/datepicker/datepicker.component.ts
+++ src/lib/core/components/datepicker/datepicker.component.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { distinctUntilChanged, map } from 'rxjs/operators';
+import { distinctUntilChanged, map, take } from 'rxjs/operators';
+import { isNullOrUndefined } from 'src/lib/helpers/is-null-or-undefined.helper';
 
 import { dayInMs } from './../../../constants/day-in-ms.const';
 import { dateClearTime } from './../../../helpers/date-clear-time.helper';
 import { getDaysInMonth } from './../../../helpers/get-days-in-month.helper';
 
 export type DatepickerSelectionMode = 'date' | 'range';
+export enum DayOfWeek {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday
+}
 
 @Component({
   selector: 'pupa-datepicker',
@@ -15,27 +25,105 @@ export type DatepickerSelectionMode = 'date' | 'range';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DatepickerComponent {
-  @Input() public selectionMode: DatepickerSelectionMode = 'date';
+  public readonly weekDayNames: Set<string> = new Set<string>(['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']);
 
-  private readonly currentDate$: BehaviorSubject<Date> = new BehaviorSubject<Date>(dateClearTime(new Date()));
-  public readonly primaryRangeStartDate$: Observable<Date> = this.currentDate$.pipe(
+  @Input() public selectionMode: DatepickerSelectionMode = 'date';
+  @Input() public set selectedDate(newValue: Date) {
+    this.selectedDate$.next(dateClearTime(newValue));
+  }
+
+  @Output() public readonly date: EventEmitter<Date> = new EventEmitter<Date>();
+
+  public readonly selectedDate$: BehaviorSubject<Date> = new BehaviorSubject<Date>(null);
+  private readonly baseDate$: BehaviorSubject<Date> = new BehaviorSubject<Date>(dateClearTime(new Date()));
+
+  public readonly primaryRangeStartDate$: Observable<Date> = this.baseDate$.pipe(
     distinctUntilChanged(),
-    map((currentDate: Date) => {
-      const currentMonthDay: number = currentDate.getDate();
-      const currentDateMs: number = currentDate.valueOf();
-      return Object.is(currentMonthDay, 1) ? currentDateMs : currentDateMs - currentMonthDay * dayInMs;
+    map((baseDate: Date) => {
+      const baseMonthDay: number = baseDate.getDate();
+      const baseDateMs: number = baseDate.valueOf();
+      return Object.is(baseMonthDay, 1) ? baseDateMs : baseDateMs - (baseMonthDay - 1) * dayInMs;
     }),
     map((rangeStartDateMs: number) => dateClearTime(new Date(rangeStartDateMs)))
   );
-  public readonly primaryRangeEndDate$: Observable<Date> = this.primaryRangeStartDate$.pipe(
+
+  public readonly primaryRangeDates$: Observable<Date[]> = this.primaryRangeStartDate$.pipe(
     distinctUntilChanged(),
     map((rangeStartDate: Date) => {
       const daysInMonth: number = getDaysInMonth(rangeStartDate);
       const rangeStartDateMs: number = rangeStartDate.valueOf();
-      return rangeStartDateMs + (daysInMonth - 1) * dayInMs;
+      return new Array(daysInMonth)
+        .fill(rangeStartDateMs)
+        .map((startDateMs: number, dayInMonth: number) => startDateMs + dayInMonth * dayInMs);
     }),
-    map((rangeEndDateMs: number) => dateClearTime(new Date(rangeEndDateMs)))
+    map((rangeDatesMs: number[]) => rangeDatesMs.map((dateMs: number) => dateClearTime(new Date(dateMs))))
   );
 
-  public readonly weekDayNames: Set<string> = new Set<string>(['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']);
+  public readonly primaryRangeLeftOffsetDates$: Observable<Date[]> = this.primaryRangeStartDate$.pipe(
+    distinctUntilChanged(),
+    map((rangeStartDate: Date) => {
+      const rangeStartDateMs: number = rangeStartDate.valueOf();
+      const lastDayOfPreviousMonthMs: number = rangeStartDateMs - dayInMs;
+      return dateClearTime(new Date(lastDayOfPreviousMonthMs));
+    }),
+    map((previousMonthLastDate: Date) => {
+      const previousMonthLastDateDayOfWeek: DayOfWeek = previousMonthLastDate.getDay();
+      if (previousMonthLastDateDayOfWeek === DayOfWeek.Sunday) {
+        return [];
+      }
+      const previousMonthLastDateMs: number = previousMonthLastDate.valueOf();
+      const visibleDaysCount: number = previousMonthLastDateDayOfWeek;
+      return new Array(visibleDaysCount)
+        .fill(previousMonthLastDateMs)
+        .map((lastMonthDateMs: number, multiplier: number) => lastMonthDateMs - multiplier * dayInMs);
+    }),
+    map((previousMonthDatesMs: number[]) => [...previousMonthDatesMs].reverse()),
+    map((reversedPreviousMonthDatesMs: number[]) =>
+      reversedPreviousMonthDatesMs.map((dateMs: number) => dateClearTime(new Date(dateMs)))
+    )
+  );
+
+  public switchToPreviousMonth(event: MouseEvent): void {
+    event.stopPropagation();
+    this.baseDate$
+      .pipe(
+        take(1),
+        map((currentBaseDate: Date) => {
+          const baseDateMonthDay: number = currentBaseDate.getDate();
+          const currentBaseDateMs: number = currentBaseDate.valueOf();
+          return currentBaseDateMs - baseDateMonthDay * dayInMs;
+        }),
+        map((newBaseDateMs: number) => dateClearTime(new Date(newBaseDateMs)))
+      )
+      .subscribe((newBaseDate: Date) => this.baseDate$.next(newBaseDate));
+  }
+
+  public switchToNextMonth(event: MouseEvent): void {
+    event.stopPropagation();
+    this.baseDate$
+      .pipe(
+        take(1),
+        map((currentBaseDate: Date) => {
+          const baseDateMonthDay: number = currentBaseDate.getDate();
+          const currentBaseDateMs: number = currentBaseDate.valueOf();
+          const currentBaseDateMonthDaysCount: number = getDaysInMonth(currentBaseDate);
+          return currentBaseDateMs + (currentBaseDateMonthDaysCount + 1 - baseDateMonthDay) * dayInMs;
+        }),
+        map((newBaseDateMs: number) => dateClearTime(new Date(newBaseDateMs)))
+      )
+      .subscribe((newBaseDate: Date) => this.baseDate$.next(newBaseDate));
+  }
+
+  public selectDate(date: Date, event: MouseEvent): void {
+    event.stopPropagation();
+    this.selectedDate$.next(date);
+    console.log(this.selectedDate$.value);
+  }
+
+  public readonly isSameDate = (dateA: Date, dateB: Date): boolean => {
+    if (isNullOrUndefined(dateA) || isNullOrUndefined(dateB)) {
+      return false;
+    }
+    return Object.is(dateA.valueOf(), dateB.valueOf());
+  };
 }
