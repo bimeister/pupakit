248c84b09a8fdaed7bfe2f6c99fb8dde6b5b8437
Fedor Usakov <fedor.usakov@meistersoft.ru>
2019-09-19 18:41:16 +0300

    add range selector

--- src/lib/core/components/datepicker/datepicker.component.ts
+++ src/lib/core/components/datepicker/datepicker.component.ts
@@ -1,6 +1,14 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { distinctUntilChanged, map, take } from 'rxjs/operators';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnDestroy,
+  Output
+} from '@angular/core';
+import { BehaviorSubject, combineLatest, Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged, filter, map, shareReplay, skipWhile, take } from 'rxjs/operators';
 import { isNullOrUndefined } from 'src/lib/helpers/is-null-or-undefined.helper';
 
 import { dayInMs } from './../../../constants/day-in-ms.const';
@@ -24,46 +32,74 @@ export enum DayOfWeek {
   styleUrls: ['./datepicker.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DatepickerComponent {
+export class DatepickerComponent implements OnDestroy {
   public readonly weekDayNames: Set<string> = new Set<string>(['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']);
 
-  @Input() public selectionMode: DatepickerSelectionMode = 'date';
+  @Input() public selectionMode: DatepickerSelectionMode = 'range';
   @Input() public set selectedDate(newValue: Date) {
     this.selectedDate$.next(dateClearTime(newValue));
   }
 
   @Output() public readonly date: EventEmitter<Date> = new EventEmitter<Date>();
+  @Output() public readonly range: EventEmitter<[Date, Date]> = new EventEmitter<[Date, Date]>();
 
+  public get isDateSelectionMode(): boolean {
+    return this.selectionMode === 'date';
+  }
+
+  public readonly selectedRange$: BehaviorSubject<Date[]> = new BehaviorSubject<Date[]>([]);
   public readonly selectedDate$: BehaviorSubject<Date> = new BehaviorSubject<Date>(null);
+  public readonly hoveredDate$: BehaviorSubject<Date> = new BehaviorSubject<Date>(null);
+
+  public readonly hoveredRange$: Observable<Date[]> = combineLatest([this.selectedRange$, this.hoveredDate$]).pipe(
+    filter((aggregatedDates: [Date[], Date]) => {
+      const selectedRange: Date[] = aggregatedDates[0];
+      const invalidRangeSize: number = 2;
+      return !Object.is(selectedRange.length, invalidRangeSize);
+    }),
+    map((aggregatedDates: [Date[], Date]) => {
+      const selectedRange: Date[] = aggregatedDates[0];
+      if (Object.is(selectedRange.length, 0)) {
+        return [];
+      }
+      const selectedDateFromRange: Date = selectedRange[selectedRange.length - 1];
+      const hoveredDate: Date = aggregatedDates[1];
+      return [selectedDateFromRange, hoveredDate];
+    }),
+    shareReplay(1)
+  );
+
   private readonly baseDate$: BehaviorSubject<Date> = new BehaviorSubject<Date>(dateClearTime(new Date()));
 
-  public readonly primaryRangeStartDate$: Observable<Date> = this.baseDate$.pipe(
+  private readonly subscription: Subscription = new Subscription();
+
+  public readonly primarySectionStartDate$: Observable<Date> = this.baseDate$.pipe(
     distinctUntilChanged(),
     map((baseDate: Date) => {
       const baseMonthDay: number = baseDate.getDate();
       const baseDateMs: number = baseDate.valueOf();
       return Object.is(baseMonthDay, 1) ? baseDateMs : baseDateMs - (baseMonthDay - 1) * dayInMs;
     }),
-    map((rangeStartDateMs: number) => dateClearTime(new Date(rangeStartDateMs)))
+    map((sectionStartDateMs: number) => dateClearTime(new Date(sectionStartDateMs)))
   );
 
-  public readonly primaryRangeDates$: Observable<Date[]> = this.primaryRangeStartDate$.pipe(
+  public readonly primarySectionDates$: Observable<Date[]> = this.primarySectionStartDate$.pipe(
     distinctUntilChanged(),
-    map((rangeStartDate: Date) => {
-      const daysInMonth: number = getDaysInMonth(rangeStartDate);
-      const rangeStartDateMs: number = rangeStartDate.valueOf();
+    map((sectionStartDate: Date) => {
+      const daysInMonth: number = getDaysInMonth(sectionStartDate);
+      const sectionStartDateMs: number = sectionStartDate.valueOf();
       return new Array(daysInMonth)
-        .fill(rangeStartDateMs)
+        .fill(sectionStartDateMs)
         .map((startDateMs: number, dayInMonth: number) => startDateMs + dayInMonth * dayInMs);
     }),
-    map((rangeDatesMs: number[]) => rangeDatesMs.map((dateMs: number) => dateClearTime(new Date(dateMs))))
+    map((sectionDatesMs: number[]) => sectionDatesMs.map((dateMs: number) => dateClearTime(new Date(dateMs))))
   );
 
-  public readonly primaryRangeLeftOffsetDates$: Observable<Date[]> = this.primaryRangeStartDate$.pipe(
+  public readonly primarySectionLeftOffsetDates$: Observable<Date[]> = this.primarySectionStartDate$.pipe(
     distinctUntilChanged(),
-    map((rangeStartDate: Date) => {
-      const rangeStartDateMs: number = rangeStartDate.valueOf();
-      const lastDayOfPreviousMonthMs: number = rangeStartDateMs - dayInMs;
+    map((sectionStartDate: Date) => {
+      const sectionStartDateMs: number = sectionStartDate.valueOf();
+      const lastDayOfPreviousMonthMs: number = sectionStartDateMs - dayInMs;
       return dateClearTime(new Date(lastDayOfPreviousMonthMs));
     }),
     map((previousMonthLastDate: Date) => {
@@ -82,6 +118,41 @@ export class DatepickerComponent {
       reversedPreviousMonthDatesMs.map((dateMs: number) => dateClearTime(new Date(dateMs)))
     )
   );
+
+  constructor() {
+    this.subscription
+      .add(
+        this.selectedDate$
+          .pipe(
+            skipWhile(() => !this.isDateSelectionMode),
+            filter((selectedDate: Date) => !isNullOrUndefined(selectedDate)),
+            distinctUntilChanged(this.isSameDate)
+          )
+          .subscribe((selectedDate: Date) => this.date.emit(selectedDate))
+      )
+      .add(
+        this.selectedRange$
+          .pipe(
+            skipWhile(() => this.isDateSelectionMode),
+            filter((selectedRangeDates: Date[]) => {
+              const validDatesCount: number = 2;
+              return Array.isArray(selectedRangeDates) && Object.is(selectedRangeDates.length, validDatesCount);
+            })
+          )
+          .subscribe((selectedRange: [Date, Date]) => this.range.emit(selectedRange))
+      );
+  }
+
+  @HostListener('window:click')
+  public processWindowClick(): void {
+    this.selectedDate$.next(null);
+    this.selectedRange$.next([]);
+    this.hoveredDate$.next(null);
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   public switchToPreviousMonth(event: MouseEvent): void {
     event.stopPropagation();
@@ -114,10 +185,30 @@ export class DatepickerComponent {
       .subscribe((newBaseDate: Date) => this.baseDate$.next(newBaseDate));
   }
 
-  public selectDate(date: Date, event: MouseEvent): void {
+  public processDateSelection(date: Date, event: MouseEvent): void {
     event.stopPropagation();
-    this.selectedDate$.next(date);
-    console.log(this.selectedDate$.value);
+    if (this.isDateSelectionMode) {
+      this.selectedDate$.next(date);
+      return;
+    }
+    this.selectedRange$
+      .pipe(
+        take(1),
+        map((alreadySelectedDates: Date[]) => {
+          return Object.is(alreadySelectedDates.length, 1)
+            ? [alreadySelectedDates[alreadySelectedDates.length - 1]]
+            : [];
+        })
+      )
+      .subscribe((alreadySelectedDates: Date[]) => this.selectedRange$.next([...alreadySelectedDates, date]));
+  }
+
+  public processDateHover(date: Date, event: MouseEvent): void {
+    event.stopPropagation();
+    if (this.isDateSelectionMode) {
+      return;
+    }
+    this.hoveredDate$.next(date);
   }
 
   public readonly isSameDate = (dateA: Date, dateB: Date): boolean => {
@@ -125,5 +216,39 @@ export class DatepickerComponent {
       return false;
     }
     return Object.is(dateA.valueOf(), dateB.valueOf());
+  };
+
+  public readonly dateIsInDateRange = (date: Date, dateRange: Date[]): boolean => {
+    if (isNullOrUndefined(date) || !Array.isArray(dateRange) || Object.is(dateRange.length, 0)) {
+      return false;
+    }
+    const uniqueRangeItemsMs: Set<number> = new Set<number>(dateRange.map((rangeItem: Date) => rangeItem.valueOf()));
+    const validRangeSize: number = 2;
+    const rangeIsInvalid: boolean = !Object.is(uniqueRangeItemsMs.size, validRangeSize);
+    if (rangeIsInvalid) {
+      return false;
+    }
+    const rangeStartDateIndex: number = dateRange.findIndex(
+      (rangeItem: Date, rangeItemIndex: number, rangeItemOrigin: [Date, Date]) => {
+        const nextItem: Date = rangeItemOrigin[rangeItemIndex + 1];
+        const previousItem: Date = rangeItemOrigin[rangeItemIndex - 1];
+        if (isNullOrUndefined(previousItem)) {
+          return rangeItem.valueOf() < nextItem.valueOf();
+        }
+        return rangeItem.valueOf() < previousItem.valueOf();
+      }
+    );
+    const rangeEndDateIndex: number = Object.is(rangeStartDateIndex, 0) ? 1 : 0;
+    const rangeStartDateMs: number = dateRange[rangeStartDateIndex].valueOf();
+    const rangeEndDateMs: number = dateRange[rangeEndDateIndex].valueOf();
+    const dateToTestMs: number = date.valueOf();
+    return rangeStartDateMs < dateToTestMs && rangeEndDateMs > dateToTestMs;
+  };
+
+  public readonly dateIsInDateArray = (date: Date, dateArray: Date[]): boolean => {
+    if (isNullOrUndefined(date) || !Array.isArray(dateArray) || Object.is(dateArray.length, 0)) {
+      return false;
+    }
+    return dateArray.some((dateFromRange: Date) => this.isSameDate(dateFromRange, date));
   };
 }
