134081cefd3bc6699fb9642f6a30a54a7558707f
Fedor Usakov <fedor.usakov@meistersoft.ru>
2019-09-19 15:31:55 +0300

    add date selector

--- src/lib/helpers/date-clear-time.helper.ts
+++ src/lib/helpers/date-clear-time.helper.ts
@@ -1,4 +1,9 @@
+import { isNullOrUndefined } from './is-null-or-undefined.helper';
+
 export const dateClearTime = (date: Date): Date => {
+  if (isNullOrUndefined(date)) {
+    return date;
+  }
   const sanitizedDate: Date = date;
   sanitizedDate.setHours(0, 0, 0, 0);
   return sanitizedDate;
