dcc86b4de9de2e274c7884803606bdafcb053a55
usachev-dev <usachev.dev@gmail.com>
2019-10-17 12:46:45 +0300

    Add components to public api, make string value property on Input

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,4 +1,30 @@
 /*
  * Public API Surface of pupakit
  */
+export { ButtonComponent } from './lib/core/components/button/button.component';
+export { InputComponent } from './lib/core/components/input/input.component';
+export { CheckboxComponent } from './lib/core/components/checkbox/checkbox.component';
+export { SpinnerComponent } from './lib/core/components/spinner/spinner.component';
+export { TileComponent } from './lib/core/components/tile/tile.component';
+export { ControlTextComponent } from './lib/core/components/control-text/control-text.component';
+export { LoaderComponent } from './lib/core/components/loader/loader.component';
+export { DropdownComponent } from './lib/core/components/dropdown/dropdown.component';
+export { SelectComponent } from './lib/core/components/select/select.component';
+export { IconButtonComponent } from './lib/core/components/icon-button/icon-button.component';
+export { SearchFieldComponent } from './lib/core/components/search-field/search-field.component';
+export { SwitcherComponent } from './lib/core/components/switcher/switcher.component';
+export { DroppableComponent } from './lib/core/components/droppable/droppable.component';
+export { RatingComponent } from './lib/core/components/rating/rating.component';
+export { DaySelectorComponent } from './lib/core/components/day-selector/day-selector.component';
+export { WasherPanelComponent } from './lib/core/components/washer-panel/washer-panel.component';
+export { ChipComponent } from './lib/core/components/chip/chip.component';
+export { ChipButtonComponent } from './lib/core/components/chip-button/chip-button.component';
+export { ChipSelectComponent } from './lib/core/components/chip-select/chip-select.component';
+export { TreeComponent } from './lib/core/components/tree/tree.component';
+export { TreeNodeComponent } from './lib/core/components/tree/tree-node/tree-node.component';
+export { ModalComponent } from './lib/core/components/modal/modal.component';
+export { DatepickerComponent } from './lib/core/components/datepicker/datepicker.component';
+export { TabsComponent } from './lib/core/components/tabs/tabs.component';
+export { DatagridComponent } from './lib/core/components/datagrid/datagrid.component';
+
 export { PupakitCore } from './lib/core/pupakit-core.module';
