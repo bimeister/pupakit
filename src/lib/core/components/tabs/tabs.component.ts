d7310d4e57c758727ac40fef351ea92dd101c759
ScarletFlash <scarletflash.dev@gmail.com>
2019-11-11 20:02:42 +0300

    fix tabs detecting child routes

--- src/lib/core/components/tabs/tabs.component.ts
+++ src/lib/core/components/tabs/tabs.component.ts
@@ -11,8 +11,8 @@ import {
   ViewChildren
 } from '@angular/core';
 import { NavigationEnd, Router, RouterEvent } from '@angular/router';
-import { BehaviorSubject, combineLatest, fromEvent, merge, Observable, Subscription } from 'rxjs';
-import { delay, filter, first, map } from 'rxjs/operators';
+import { BehaviorSubject, fromEvent, Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged, distinctUntilKeyChanged, filter, map, startWith, switchMap, take } from 'rxjs/operators';
 
 import { isNullOrUndefined } from './../../../helpers/is-null-or-undefined.helper';
 
@@ -30,55 +30,87 @@ export interface Tab {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TabsComponent implements OnDestroy, AfterViewInit {
-  @ViewChild('highlighterElement', { static: true }) public readonly highlighterElementRef: ElementRef<HTMLDivElement>;
-  @ViewChild('tabsElement', { static: false }) public readonly tabsElementRef: ElementRef<HTMLDivElement>;
-  @ViewChildren('tabElement') public readonly tabElements: QueryList<ElementRef<HTMLButtonElement>>;
+  @ViewChild('highlighterElement', { static: true }) private readonly highlighterElementRef: ElementRef<HTMLDivElement>;
+  @ViewChild('tabsElement', { static: false }) private readonly tabsElementRef: ElementRef<HTMLDivElement>;
+  @ViewChildren('tabsItemElement') private readonly tabsItemsRef: QueryList<ElementRef<HTMLButtonElement>>;
 
-  public readonly tabs$: BehaviorSubject<Tab[]> = new BehaviorSubject([]);
-  @Input() public set tabs(value: Tab[]) {
-    this.tabs$.next(value);
+  @Input() public set tabs(newValue: Tab[]) {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+    this.tabs$.next(newValue);
   }
 
-  private readonly initialUrl$: BehaviorSubject<string> = new BehaviorSubject<string>(null);
+  public readonly tabs$: BehaviorSubject<Tab[]> = new BehaviorSubject<Tab[]>([]);
 
-  public readonly activeUrl$: Observable<string> = merge(
-    this.initialUrl$,
-    this.router.events.pipe(
-      filter((event: RouterEvent) => event instanceof NavigationEnd),
-      map((event: NavigationEnd) => event.urlAfterRedirects)
-    )
+  private readonly sortedTabsRoutes$: Observable<string[]> = this.tabs$.pipe(
+    map((tabs: Tab[]) => this.sortTabsByRouteLengthDescendingly(tabs)),
+    map((tabs: Tab[]) => tabs.map((tab: Tab) => tab.route.toLowerCase())),
+    distinctUntilKeyChanged('length')
   );
 
-  public readonly selectedTabIndex$: Observable<number> = combineLatest([this.tabs$, this.activeUrl$]).pipe(
-    map(([tabs, url]: [Tab[], string]) => {
-      const regex: RegExp = new RegExp(`^${url}`);
-      return tabs.findIndex((tab: Tab) => regex.test(tab.route) || tab.route === url);
-    })
+  private get currentUrl(): string {
+    const wholeUrl: string = window.location.href;
+    const host: string = window.location.host;
+    const protocol: string = `${window.location.protocol}//`;
+    return wholeUrl
+      .replace(host, '')
+      .replace(protocol, '')
+      .toLowerCase();
+  }
+
+  private readonly routerNavigationEnded$: Observable<void> = this.router.events.pipe(
+    startWith(new NavigationEnd(null, null, this.currentUrl)),
+    filter((routerEvent: RouterEvent) => routerEvent instanceof NavigationEnd),
+    distinctUntilKeyChanged('url'),
+    map(() => undefined)
   );
 
-  public readonly selectedTabElement$: Observable<HTMLElement> = this.selectedTabIndex$.pipe(
-    delay(0),
-    map(
-      (tabIndex: number): HTMLElement => {
-        const elementRef: ElementRef<HTMLButtonElement> = this.tabElements.toArray()[tabIndex];
-        return elementRef ? elementRef.nativeElement : null;
-      }
-    )
+  private readonly activeUrl$: Observable<string> = this.routerNavigationEnded$.pipe(
+    map(() => this.currentUrl),
+    distinctUntilChanged()
   );
 
-  private readonly resize$: Observable<unknown> = merge(fromEvent(window, 'resize'), this.initialUrl$);
+  public readonly selectedTabIndex$: Observable<number> = this.activeUrl$.pipe(
+    switchMap((currentUrl: string) => {
+      return this.sortedTabsRoutes$.pipe(
+        map((tabRoutes: string[]) => {
+          const targetTabIndexSorted: number = this.getSelectedTabIndex(currentUrl, tabRoutes);
+          return tabRoutes[targetTabIndexSorted];
+        }),
+        filter((tabRoute: string) => !isNullOrUndefined(tabRoute)),
+        map((tabRoute: string) => tabRoute.toLowerCase()),
+        switchMap((selectedTabRoute: string) =>
+          this.tabs$.pipe(
+            map((tabs: Tab[]) => {
+              return tabs.findIndex((tab: Tab) => tab.route.toLowerCase() === selectedTabRoute);
+            })
+          )
+        )
+      );
+    }),
+    distinctUntilChanged()
+  );
+
+  private readonly selectedTabElement$: Observable<HTMLElement> = this.selectedTabIndex$.pipe(
+    map((tabIndex: number) => this.tabsItemsRef.toArray()[tabIndex]),
+    filter((selectedTabElement: ElementRef<HTMLElement>) => !isNullOrUndefined(selectedTabElement)),
+    map((selectedTabElement: ElementRef<HTMLElement>) => selectedTabElement.nativeElement)
+  );
+
+  private readonly resizeEvent$: Observable<Event> = fromEvent(window, 'resize');
 
   private readonly subscription: Subscription = new Subscription();
 
   constructor(private readonly renderer: Renderer2, private readonly router: Router) {}
 
   public ngAfterViewInit(): void {
-    this.subscription.add(
-      combineLatest([this.selectedTabElement$, this.resize$]).subscribe(([selectedTabElement, _]: [Element, unknown]) =>
-        this.highlightTabElement(selectedTabElement)
-      )
-    );
-    this.initialUrl$.next(this.router.url);
+    this.rerenderHighlighterOnSelectedElementChange();
+    this.rerenderHighlighterOnResizeEvent();
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   public selectTab(tab: Tab, clickEvent?: MouseEvent): void {
@@ -87,13 +119,22 @@ export class TabsComponent implements OnDestroy, AfterViewInit {
     }
     this.tabs$
       .pipe(
-        first(),
+        take(1),
         filter((tabs: Tab[]) => !isNullOrUndefined(tabs)),
         filter((tabs: Tab[]) => tabs.some((tabInner: Tab) => tab.route === tabInner.route))
       )
-      .subscribe(() => {
-        this.router.navigateByUrl(tab.route);
-      });
+      .subscribe(() => this.router.navigateByUrl(tab.route));
+  }
+
+  private rerenderHighlighterOnSelectedElementChange(): void {
+    this.subscription.add(this.selectedTabElement$.subscribe((element: Element) => this.highlightTabElement(element)));
+  }
+
+  private rerenderHighlighterOnResizeEvent(): void {
+    const elementToHighlight$: Observable<Element> = this.resizeEvent$.pipe(
+      switchMap(() => this.selectedTabElement$.pipe(take(1)))
+    );
+    this.subscription.add(elementToHighlight$.subscribe((element: Element) => this.highlightTabElement(element)));
   }
 
   private highlightTabElement(selectedTabElement: Element): void {
@@ -114,7 +155,39 @@ export class TabsComponent implements OnDestroy, AfterViewInit {
     this.renderer.setStyle(highlighter, 'width', `${selectedTabWidthPx}px`);
   }
 
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  private readonly getSelectedTabIndex = (currentUrl: string, tabRoutes: string[]): number => {
+    const fullOccuranceIndex: number = tabRoutes.indexOf(currentUrl);
+    if (!Object.is(fullOccuranceIndex, -1)) {
+      return fullOccuranceIndex;
+    }
+    const urlWithoutParams: string = this.getUrlStringWithoutParams(currentUrl);
+    return tabRoutes.findIndex((tabRoute: string) => urlWithoutParams.startsWith(tabRoute.toLowerCase()));
+  };
+
+  private readonly getUrlStringWithoutParams = (url: string): string => {
+    const indexOfQueryParamsStart: number = url.indexOf('?');
+    if (Object.is(indexOfQueryParamsStart, -1)) {
+      return url.toLowerCase();
+    }
+    return url.toLowerCase().substring(0, indexOfQueryParamsStart);
+  };
+
+  private readonly sortTabsByRouteLengthDescendingly = (tabs: Tab[]): Tab[] => {
+    if (!Array.isArray(tabs) || Object.is(tabs.length, 0)) {
+      return [];
+    }
+    return [...tabs]
+      .sort((tab1: Tab, tab2: Tab): -1 | 0 | 1 => {
+        const tab1RouteLength: number = tab1.route.length;
+        const tab2RouteLength: number = tab2.route.length;
+        if (tab1RouteLength > tab2RouteLength) {
+          return 1;
+        }
+        if (tab1RouteLength < tab2RouteLength) {
+          return -1;
+        }
+        return 0;
+      })
+      .reverse();
+  };
 }
