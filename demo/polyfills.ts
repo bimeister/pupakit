ff7cda26a0c21a7f3e6eae8fea7b6358a635937d
ScarletFlash <scarletflash.dev@gmail.com>
2019-11-13 19:24:26 +0300

    rebuild tabs for query params

--- demo/polyfills.ts
+++ demo/polyfills.ts
@@ -20,6 +20,8 @@
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'zone.js/dist/zone-evergreen';
+
 
 /**
  * Web Animations `@angular/platform-browser/animations`
