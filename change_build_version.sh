a9a63e09f65e762287e25e42df59d6462e242ed8
ScarletFlash <scarletflash.dev@gmail.com>
2020-01-22 13:20:15 +0300

    add .gitlab-ci.yml

--- change_build_version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -xe
-
-cat dist/@meistersoft/pupakit/package.json | jq ".version=\"${PKG_VERSION}\"" > dist/@meistersoft/pupakit/temp.json && mv dist/@meistersoft/pupakit/temp.json dist/@meistersoft/pupakit/package.json
