c9f399648224ab97212beb41a09de19b47c24d7d
ScarletFlash <scarletflash.dev@gmail.com>
2020-02-28 11:21:53 +0300

    fix module imports

--- src/lib/components/input/input.module.ts
+++ src/lib/components/input/input.module.ts
@@ -1,12 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { ChipSelectModule } from '../chip-select/chip-select.module';
+import { DatepickerModule } from '../datepicker/datepicker.module';
+import { DroppableModule } from '../droppable/droppable.module';
 import { SharedModule } from './../../../internal/shared/shared.module';
 import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [InputComponent],
-  imports: [SharedModule, ChipSelectModule],
+  imports: [SharedModule, ChipSelectModule, DatepickerModule, DroppableModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [InputComponent]
 })
