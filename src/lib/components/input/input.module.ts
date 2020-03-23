f683968183ef5c8f23e7ace2e53709ad9e33121f
progikusok <moyomylo11@gmail.com>
2020-03-23 19:51:12 +0300

    bugfix/BIM-4567: imports in input and chip-select-tree

--- src/lib/components/input/input.module.ts
+++ src/lib/components/input/input.module.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
-import { ChipSelectModule } from '../chip-select/chip-select.module';
 import { DatepickerModule } from '../datepicker/datepicker.module';
 import { DroppableModule } from '../droppable/droppable.module';
 import { SharedModule } from './../../../internal/shared/shared.module';
@@ -8,7 +7,7 @@ import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [InputComponent],
-  imports: [SharedModule, ChipSelectModule, DatepickerModule, DroppableModule],
+  imports: [SharedModule, DatepickerModule, DroppableModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [InputComponent]
 })
