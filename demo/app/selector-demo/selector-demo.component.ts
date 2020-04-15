911cfe943b0d3d8f6b5d413c2609c14bf8cc7273
ScarletFlash <scarletflash.dev@gmail.com>
2020-04-15 16:45:39 +0300

    design events
    add selection clear feature
    
    lint
    
    add clear btn side selection
    fix clrbtn animations
    
    lint
    
    rename misstap
    
    fix api imports
    
    cr
    
    cr
    
    add clear btn hiding move rtl and ltr
    
    add selectedItems observ
    add item value
    
    add component form support
    add demo form
    
    add demo actions
    add disabled states for items and clr btn
    fix touch
    
    lint
    
    add ngControl getter
    
    disable demo lint
    
    add demo examples
    better name for hide left and right
    move clr btn pointer disable in disabled class
    clr btn dieabled by default
    check form events for undef
    add conditional selection clear
    
    lint
    
    add demo tests
    
    remove clear button (
    
    leave only new demo tests
    remove validition
    
    add new colors
    
    cr
    
    fix unused injection
    
    lint
    
    lint
    
    cr
    
    cr

--- demo/app/selector-demo/selector-demo.component.ts
+++ demo/app/selector-demo/selector-demo.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'demo-select',
@@ -6,4 +7,46 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './selector-demo.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectorDemoComponent {}
+export class SelectorDemoComponent {
+  public readonly sampleFormControl: FormControl = new FormControl('default value', Validators.required);
+  public readonly daysOfWeek: string[] = ['Пн', 'Вт', 'Среда', 'Воскресенье', 'a', 'b'];
+
+  constructor() {
+    this.sampleFormControl.valueChanges.subscribe(v => {
+      // tslint:disable-next-line: no-console
+      console.log('sd', v);
+      // tslint:disable-next-line: no-console
+      console.log('sd errs', this.sampleFormControl.errors);
+    });
+  }
+
+  public writeTestValue(): void {
+    this.sampleFormControl.setValue([0, 1, 3]);
+  }
+  public selectRandom(): void {
+    const toSelect: number[] = [];
+
+    const rndInt = (max: number) => Math.floor(Math.random() * max);
+
+    for (let i: number = 0; i < 10; i++) {
+      toSelect.push(rndInt(10));
+    }
+
+    // tslint:disable-next-line: no-console
+    console.log(toSelect);
+    this.sampleFormControl.setValue(toSelect);
+  }
+
+  public disableForm(): void {
+    this.sampleFormControl.enabled ? this.sampleFormControl.disable() : this.sampleFormControl.enable();
+  }
+
+  public clearAll(): void {
+    this.sampleFormControl.setValue([]);
+  }
+
+  public validate(): void {
+    // tslint:disable-next-line: no-console
+    console.log('sd validate', this.sampleFormControl.errors);
+  }
+}
