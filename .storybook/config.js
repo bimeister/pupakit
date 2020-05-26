4233a1862eba8635137687d4a476750dc1d74dfd
ScarletFlash <scarletflash.dev@gmail.com>
2020-05-26 12:27:02 +0300

    upgrade(dependencies)

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-// import '!style-loader!css-loader!../src/styles/styles.scss';
-import { configure } from '@storybook/angular';
-
-const requireContext = require.context('../src/lib/', true, /\.stories\.ts$/);
-function loadStories() {
-  requireContext.keys().forEach(filename => requireContext(filename));
-}
-
-configure(loadStories, module);
