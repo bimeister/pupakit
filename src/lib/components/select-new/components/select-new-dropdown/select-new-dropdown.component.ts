72dea0ba67fd3d3fda35f88bc253f40066bfdc44
ScarletFlash <scarletflash.dev@gmail.com>
2020-07-22 13:06:30 +0300

    refactor(select animations)

--- src/lib/components/select-new/components/select-new-dropdown/select-new-dropdown.component.ts
+++ src/lib/components/select-new/components/select-new-dropdown/select-new-dropdown.component.ts
@@ -1,8 +1,8 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CdkOverlayOrigin } from '@angular/cdk/overlay';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable, of, timer } from 'rxjs';
-import { filter, mapTo, switchMap, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { distinctUntilChanged, filter, map } from 'rxjs/operators';
 
 import { isNullOrUndefined } from '../../../../../internal/helpers/is-null-or-undefined.helper';
 import { SelectNewStateService } from '../../services/select-new-state.service';
@@ -17,19 +17,20 @@ const ANIMATION_DURATION_MS: number = 150;
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
     trigger('dropdownExpanded', [
-      state('void', style({ height: 0 })),
-      state('expanded', style({ height: '*' })),
-      transition('* => *', [animate(`${ANIMATION_DURATION_MS}ms ease`)])
+      state('void', style({ height: 0, margin: '0', transform: 'translateY(-8px)' })),
+      state('expanded', style({ height: '*', margin: '4px 0', transform: 'translateY(0)' })),
+      transition('void => expanded', [animate(`${ANIMATION_DURATION_MS}ms ease`)]),
+      transition('expanded => void', [animate(`${ANIMATION_DURATION_MS}ms ease`)])
     ])
   ]
 })
 export class SelectNewDropdownComponent<T> {
   public readonly isExpanded$: Observable<boolean> = this.selectNewStateService.isExpanded$;
-  public readonly isVisible$: Observable<boolean> = this.isExpanded$.pipe(
-    switchMap((isExpanded: boolean) =>
-      isExpanded ? of(isExpanded) : timer(ANIMATION_DURATION_MS).pipe(take(1), mapTo(isExpanded))
-    )
+  public readonly animationState$: Observable<string> = this.isExpanded$.pipe(
+    distinctUntilChanged(),
+    map((isExpanded: boolean) => (isExpanded ? 'expanded' : 'void'))
   );
+
   public readonly dropDownOverlayOrigin$: Observable<
     CdkOverlayOrigin
   > = this.selectNewStateService.dropDownOverlayOrigin$.pipe(
