58fc5341b9c7832b2c3d1206c0d395830b350420
eonicum <kirill.marchenko.it@gmail.com>
2020-08-12 17:01:47 +0300

    Add grid state interface to exports

--- src/public-api.ts
+++ src/public-api.ts
@@ -32,6 +32,7 @@ export { DropdownMenuModule } from './lib/components/dropdown-menu/dropdown-menu
 export { TreeModule } from './lib/components/tree/tree.module';
 export { WasherModule } from './lib/components/washer/washer.module';
 export { VerticalTabsModule } from './lib/components/vertical-tabs/vertical-tabs.module';
+
 export * from './lib/components/api';
 export {
   DropdownItem,
@@ -51,5 +52,7 @@ export {
   DatagridManipulator,
   DatagridThemes,
   ButtonColor,
-  GridRowEvent
+  GridRowEvent,
+  GridState,
+  SortModel
 } from './internal/api';
