7b56c343421eb5eaa68c8bbd1afaef2d9a1f2337
webgangster0 <aleksey.akulov@outlook.com>
2020-09-30 19:39:38 +0300

    Add clear button to select search input.

--- src/lib/components/select-new/components/select-new-search/select-new-search.component.ts
+++ src/lib/components/select-new/components/select-new-search/select-new-search.component.ts
@@ -4,9 +4,15 @@ import {
   EventEmitter,
   HostListener,
   Input,
+  OnDestroy,
+  OnInit,
   Output,
   ViewEncapsulation
 } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { shareReplayWithRefCount } from '@meistersoft/utilities';
+import { Observable, Subscription } from 'rxjs';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'pupa-select-new-search',
@@ -15,16 +21,34 @@ import {
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectNewSearchComponent {
+export class SelectNewSearchComponent implements OnInit, OnDestroy {
   @Input() public placeholder: string = '';
   @Output() public readonly query: EventEmitter<string> = new EventEmitter<string>();
+
+  public readonly control: FormControl = new FormControl('');
+
+  public readonly query$: Observable<string> = this.control.valueChanges.pipe(startWith(''), shareReplayWithRefCount());
+
+  private readonly subscription: Subscription = new Subscription();
+
+  public ngOnInit(): void {
+    this.subscription.add(this.processInput());
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   @HostListener('click', ['$event'])
   public processClick(event: Event): void {
     event.stopPropagation();
   }
 
-  public processInput(query: string): void {
-    this.query.emit(query);
+  public resetQuery(): void {
+    this.control.setValue('');
+  }
+
+  private processInput(): Subscription {
+    return this.query$.subscribe((query: string) => this.query.emit(query));
   }
 }
