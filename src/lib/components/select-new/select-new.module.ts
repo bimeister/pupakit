3fba893fbc3320e6fedebaaff7bedc8e091b376f
Fedor Usakov <fedor.usakov@meistersoft.ru>
2020-10-06 21:30:41 +0300

    refactor(components/regular)

--- src/lib/components/select-new/select-new.module.ts
+++ src/lib/components/select-new/select-new.module.ts
@@ -3,13 +3,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { SharedModule } from '../../../internal/shared/shared.module';
 import { TreeModule } from '../tree/tree.module';
-import { SelectNewButtonComponent } from './components/select-new-button/select-new-button.component';
-import { SelectNewDropdownComponent } from './components/select-new-dropdown/select-new-dropdown.component';
-import { SelectNewItemComponent } from './components/select-new-item/select-new-item.component';
-import { SelectNewItemsContainerComponent } from './components/select-new-items-container/select-new-items-container.component';
-import { SelectNewSearchComponent } from './components/select-new-search/select-new-search.component';
-import { SelectNewTreeComponent } from './components/select-new-tree/select-new-tree.component';
-import { SelectNewComponent } from './components/select-new/select-new.component';
+import { SelectNewButtonComponent } from './components/regular/select-new-button/select-new-button.component';
+import { SelectNewDropdownComponent } from './components/regular/select-new-dropdown/select-new-dropdown.component';
+import { SelectNewItemComponent } from './components/regular/select-new-item/select-new-item.component';
+import { SelectNewItemsContainerComponent } from './components/regular/select-new-items-container/select-new-items-container.component';
+import { SelectNewSearchComponent } from './components/regular/select-new-search/select-new-search.component';
+import { SelectNewTreeComponent } from './components/regular/select-new-tree/select-new-tree.component';
+import { SelectNewComponent } from './components/regular/select-new/select-new.component';
 
 const EXPORTS: any[] = [
   SelectNewComponent,
