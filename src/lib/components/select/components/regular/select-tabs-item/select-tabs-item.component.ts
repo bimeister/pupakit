637d7d4f0298e32ee796f1cc163f1e0fbbba5273
gaevivan <6507085-gaevivan@users.noreply.gitlab.com>
2020-12-10 15:24:47 +0300

    feature(selectModule + selectDemo): BIM-9841 added select-tabs

---
+++ src/lib/components/select/components/regular/select-tabs-item/select-tabs-item.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, HostListener, Input, ViewEncapsulation } from '@angular/core';
+import { isEqual } from '@meistersoft/utilities';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SelectTabsService } from '../../../services/select-tabs-state.service';
+
+@Component({
+  selector: 'pupa-select-tabs-item',
+  templateUrl: './select-tabs-item.component.html',
+  styleUrls: ['./select-tabs-item.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SelectTabsItemComponent<T> {
+  @Input() public readonly value: T;
+
+  public readonly isPicked$: Observable<boolean> = this.selectTabsService.currentValue$.pipe(
+    map((value: T) => isEqual(value, this.value))
+  );
+
+  constructor(private readonly selectTabsService: SelectTabsService<T>) {}
+
+  public processClick(): void {
+    this.selectTabsService.currentValue$.next(this.value);
+  }
+
+  @HostListener('click', ['$event'])
+  @HostListener('touchstart', ['$event'])
+  public stopPropagation(event: Event): void {
+    event.stopPropagation();
+  }
+}
