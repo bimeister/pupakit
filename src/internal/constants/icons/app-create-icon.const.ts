d3c602bc27da60528139298abbe3210b2be2e943
Mikhail Kirichenko <mikhail.kirichenko@bimeister.com>
2020-12-15 17:30:55 +0300

    feature(SelectModule): BIM-4929 add icons

---
+++ src/internal/constants/icons/app-create-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../declarations/interfaces/icon-definition.interface';
+
+export const appCreateIcon: IconDefinition = {
+  name: 'app-create-icon',
+  data: `<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M10.0409 3.39633L1.5368 11.9213L1.02148 13.1688L2.26898 12.6535L10.794 4.14946L10.0409 3.39633ZM11.7424 1.69508L11.374 2.06321L12.1271 2.81633L12.4955 2.44789C12.5922 2.35115 12.6465 2.21997 12.6465 2.08321C12.6465 1.94644 12.5922 1.81526 12.4955 1.71852L12.4721 1.69508C12.4242 1.64717 12.3673 1.60916 12.3047 1.58322C12.2421 1.55729 12.175 1.54395 12.1073 1.54395C12.0395 1.54395 11.9724 1.55729 11.9098 1.58322C11.8472 1.60916 11.7903 1.64717 11.7424 1.69508Z" stroke="currentColor" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`
+};
