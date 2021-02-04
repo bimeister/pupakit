207b6e175bd7c1ffe9d186bf6917076a02b31945
Fedor Usakov <fedor.usakov@bimeister.com>
2021-02-04 19:02:41 +0300

    chore: remove api.ts git tracking

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,11 +1,10 @@
 import { parallel, series, task } from 'gulp';
-
+import { buildIconsTask } from './build/gulp/build-icons.task';
 import { buildLibraryTask } from './build/gulp/build-library.task';
 import { bundleAssetsTask } from './build/gulp/bundle-assets.task';
 import { bundleStyleFilesTask } from './build/gulp/bundle-style-files.task';
 import { createBarrelFilesTask } from './build/gulp/create-barrel-files.task';
 import { fixImportsTask } from './build/gulp/fix-imports.task';
-import { buildIconsTask } from './build/gulp/build-icons.task';
 
 task(
   'build',
@@ -16,5 +15,7 @@ task(
     fixImportsTask()
   )
 );
+
+task('create:barrel-files', createBarrelFilesTask());
 
 task('convert-icons-to-code', buildIconsTask());
