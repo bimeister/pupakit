7e3717ebdfa9776534d65b63a4a7d6b8488ae549
Fedor Usakov <fedor.usakov@bimeister.com>
2021-02-17 18:28:47 +0300

    refactor: remove unnecessary exports from public-apt.ts

--- src/public-api.ts
+++ src/public-api.ts
@@ -20,6 +20,7 @@ export {
   ModalRef,
   OpenedDrawer,
   OpenedModal,
+  PagedVirtualScrollArguments,
   Position,
   SelectBase,
   SelectButtonBase,
@@ -32,42 +33,8 @@ export {
   SortModel,
   Tab,
   TreeItem,
-  UploadingStatus,
-  PagedVirtualScrollArguments
+  UploadingStatus
 } from './internal/api';
 export * from './internal/constants/icons/api';
 export { ComponentsModule } from './lib/components.module';
 export * from './lib/components/api';
-export { ButtonModule } from './lib/components/button/button.module';
-export { CheckboxModule } from './lib/components/checkbox/checkbox.module';
-export { ChipButtonModule } from './lib/components/chip-button/chip-button.module';
-export { ChipTabsModule } from './lib/components/chip-tabs/chip-tabs.module';
-export { ChipModule } from './lib/components/chip/chip.module';
-export { ControlTextModule } from './lib/components/control-text/control-text.module';
-export { DatagridModule } from './lib/components/datagrid/datagrid.module';
-export { DateTimePickerModule } from './lib/components/date-time-picker/date-time-picker.module';
-export { DaySelectorModule } from './lib/components/day-selector/day-selector.module';
-export { DrawerModule } from './lib/components/drawer/drawer.module';
-export { DropdownMenuModule } from './lib/components/dropdown-menu/dropdown-menu.module';
-export { DropdownModule } from './lib/components/dropdown/dropdown.module';
-export { DroppableModule } from './lib/components/droppable/droppable.module';
-export { IconButtonModule } from './lib/components/icon-button/icon-button.module';
-export { InputModule } from './lib/components/input/input.module';
-export { PagedVirtualScrollModule } from './lib/components/paged-virtual-scroll/paged-virtual-scroll.module';
-export { LayoutModule } from './lib/components/layout/layout.module';
-export { ModalModule } from './lib/components/modal/modal.module';
-export { RadioGroupModule } from './lib/components/radio-group/radio-group.module';
-export { RatingModule } from './lib/components/rating/rating.module';
-export { SearchFieldModule } from './lib/components/search-field/search-field.module';
-export { SelectModule } from './lib/components/select/select.module';
-export { SelectorModule } from './lib/components/selector/selector.module';
-export { SpinnerModule } from './lib/components/spinner/spinner.module';
-export { SwitcherModule } from './lib/components/switcher/switcher.module';
-export { TableInputModule } from './lib/components/table-input/table-input.module';
-export { TabsModule } from './lib/components/tabs/tabs.module';
-export { TextareaModule } from './lib/components/textarea/textarea.module';
-export { TileModule } from './lib/components/tile/tile.module';
-export { TimeInputModule } from './lib/components/time-input/time-input.module';
-export { TooltipModule } from './lib/components/tooltip/tooltip.module';
-export { TreeModule } from './lib/components/tree/tree.module';
-export { VerticalTabsModule } from './lib/components/vertical-tabs/vertical-tabs.module';
