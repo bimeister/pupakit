7d5fabb4679bd22be105d42d832a3a2bc1350a6c
Alexelalexela <lilyaleksandr@yandex.ru>
2021-04-16 10:31:27 +0300

    refactor: BIM-11600 update checkbox

---
+++ src/lib/components/checkbox/components/checkbox/checkbox.component.spec.ts
@@ -0,0 +1,86 @@
+import { PuppeteerSetupVariables } from '../../../../../internal/declarations/interfaces/puppeteer-setup-variables.interface';
+import { getPuppeteerSetupVariables } from '../../../../../internal/helpers/get-puppeteer-setup-variables.helper';
+import { BoundingBox, Browser, ElementHandle, launch, Page, Viewport } from 'puppeteer';
+import { from, Observable, of } from 'rxjs';
+import { mapTo, switchMap, tap } from 'rxjs/operators';
+import { mapToVoid } from '@bimeister/utilities';
+
+const {
+  timeOutMs: TIME_OUT_MS,
+  slowMo: SLOW_MO,
+  screenshotsPath: SCREEN_SHOTS_PATH,
+  launchBrowserArguments: LAUNCH_BROWSER_ARGUMENTS,
+  ignoreDefaultArguments: IGNORE_DEFAULT_ARGUMENTS,
+  baseUrl: BASE_URL
+}: PuppeteerSetupVariables = getPuppeteerSetupVariables();
+const SCREEN_SHOT_PATH: string = `${SCREEN_SHOTS_PATH}/checkbox.screenshot.png`;
+const VIEWPORT: Viewport = { width: 1920, height: 1080 };
+
+describe('checkbox.component.ts', () => {
+  let page: Page;
+  let browser: Browser;
+
+  beforeAll((done: jest.DoneCallback) => {
+    from(
+      launch({
+        headless: true,
+        defaultViewport: VIEWPORT,
+        slowMo: SLOW_MO,
+        args: LAUNCH_BROWSER_ARGUMENTS,
+        ignoreDefaultArgs: IGNORE_DEFAULT_ARGUMENTS
+      })
+    )
+      .pipe(
+        tap((responseBrowser: Browser) => (browser = responseBrowser)),
+        switchMap((responseBrowser: Browser) => from(responseBrowser.newPage())),
+        tap((responsePage: Page) => (page = responsePage)),
+        switchMap((responsePage: Page) => from(responsePage.setViewport(VIEWPORT)).pipe(mapTo(responsePage))),
+        switchMap((responsePage: Page) => from(responsePage.goto(BASE_URL, { waitUntil: 'domcontentloaded' }))),
+        switchMap(() => from(page.goto(`${BASE_URL}/checkbox`, { waitUntil: 'domcontentloaded' })))
+      )
+      .subscribe(() => {
+        done();
+      });
+  });
+
+  it(
+    'should render page title as PupaKit',
+    (done: jest.DoneCallback) => {
+      from(page.title()).subscribe((resultTitle: string) => {
+        expect(resultTitle).toEqual('PupaKit');
+        done();
+      });
+    },
+    TIME_OUT_MS
+  );
+
+  it(
+    'should take full page screenshot',
+    (done: jest.DoneCallback) => {
+      const screenshot$: Observable<void> = from(
+        page.screenshot({ path: SCREEN_SHOT_PATH, type: 'png', fullPage: true })
+      ).pipe(mapToVoid());
+      screenshot$.subscribe(() => done());
+    },
+    TIME_OUT_MS
+  );
+
+  it(
+    'should display page`s header as switcher',
+    (done: jest.DoneCallback) => {
+      from(page.waitForSelector('h1'))
+        .pipe(switchMap(() => from(page.evaluate(() => document.querySelector('h1').textContent))))
+        .subscribe((result: string) => {
+          expect(result).toEqual('Switcher');
+          done();
+        });
+    },
+    TIME_OUT_MS
+  );
+
+  afterAll((done: jest.DoneCallback) => {
+    from(page.close())
+      .pipe(switchMap(() => from(browser.close())))
+      .subscribe(() => done());
+  });
+});
