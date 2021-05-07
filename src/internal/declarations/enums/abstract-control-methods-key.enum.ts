05510249e3713e352f99a6cff59e88cad52e5004
gaevivan <gaev93@yandex.ru>
2021-05-07 20:29:42 +0300

    fix: BIM-12459 touched processing

---
+++ src/internal/declarations/enums/abstract-control-methods-key.enum.ts
@@ -0,0 +1,8 @@
+export enum AbstractControlMethodsKey {
+  markAsTouched = 'markAsTouched',
+  markAsPristine = 'markAsPristine',
+  markAsDirty = 'markAsDirty',
+  markAsUntouched = 'markAsUntouched',
+  markAllAsTouched = 'markAllAsTouched',
+  markAsPending = 'markAsPending'
+}
--- src/lib/components/table-input/components/table-input/table-input.component.ts
+++ src/lib/components/table-input/components/table-input/table-input.component.ts
@@ -10,7 +10,10 @@ import {
 } from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
 import { isNil } from '@bimeister/utilities';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { AbstractControlWithNotifier } from '../../../../../internal/declarations/classes/abstract/abstract-control-with-notifiers.abstract';
+import { AbstractControlMethodsKey } from '../../../../../internal/declarations/enums/abstract-control-methods-key.enum';
 import { ComponentChange } from '../../../../../internal/declarations/interfaces/component-change.interface';
 import { ComponentChanges } from '../../../../../internal/declarations/interfaces/component-changes.interface';
 import { OnChangeCallback } from '../../../../../internal/declarations/types/on-change-callback.type';
@@ -25,7 +28,7 @@ import { TableInputStateService } from '../../services/table-input-state.service
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TableInputComponent<T> implements OnChanges, ControlValueAccessor {
+export class TableInputComponent<T> extends AbstractControlWithNotifier implements OnChanges, ControlValueAccessor {
   @Input() public readonly placeholder: string = '';
 
   @Input() public readonly errorTitle: string = '';
@@ -45,13 +48,20 @@ export class TableInputComponent<T> implements OnChanges, ControlValueAccessor {
   public readonly isValid$: Observable<boolean> = this.tableInputStateService.isValid$;
   public readonly isTouched$: Observable<boolean> = this.tableInputStateService.isTouched$;
 
+  private readonly subscription: Subscription = new Subscription();
+
   constructor(@Optional() ngControl: NgControl, private readonly tableInputStateService: TableInputStateService<T>) {
+    super(ngControl);
     if (isNil(ngControl)) {
       return;
     }
     ngControl.valueAccessor = this;
-
     this.tableInputStateService.setControlRef(ngControl);
+    this.subscription.add(this.setTouched());
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   public ngOnChanges(changes: ComponentChanges<this>): void {
@@ -73,6 +83,19 @@ export class TableInputComponent<T> implements OnChanges, ControlValueAccessor {
 
   public registerOnChange(onChange: OnChangeCallback<T>): void {
     this.tableInputStateService.defineOnChangeCallback(onChange);
+  }
+
+  public setTouched(): Subscription {
+    const touchedMethodKeyList: AbstractControlMethodsKey[] = [
+      AbstractControlMethodsKey.markAsTouched,
+      AbstractControlMethodsKey.markAsUntouched
+    ];
+    return this.propertyChanges$
+      .pipe(
+        filter((key: AbstractControlMethodsKey) => touchedMethodKeyList.includes(key)),
+        map((key: AbstractControlMethodsKey) => key === AbstractControlMethodsKey.markAsTouched)
+      )
+      .subscribe((touched: boolean) => this.tableInputStateService.isTouched$.next(touched));
   }
 
   public registerOnTouched(onTouched: OnTouchedCallback): void {
