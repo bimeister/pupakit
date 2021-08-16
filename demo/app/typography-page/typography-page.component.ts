bcbf985d8480e485a571db2b18f1c029025d62ba
edikdeisling <edikdeisling@gmail.com>
2021-08-16 18:34:12 +0300

    feature: BIM-15119 fix typography page

--- demo/app/typography-page/typography-page.component.ts
+++ demo/app/typography-page/typography-page.component.ts
@@ -7,4 +7,8 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TypographyPageComponent {}
+export class TypographyPageComponent {
+  public fontTypes: string[] = ['point', 'caption', 'paragraph'];
+  public fontSizes: string[] = ['header', 'large', 'medium', 'small', 'mark'];
+  public screenSizes: string[] = ['desktop', 'tablet', 'mobile'];
+}
