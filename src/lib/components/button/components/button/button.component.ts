1b6ee84234b5d33d8fba59b6062d7012e69fe6e0
progikusok <moyomylo11@gmail.com>
2021-08-20 11:13:59 +0300

    feature: BIM-15166 add loading and icons

--- src/lib/components/button/components/button/button.component.ts
+++ src/lib/components/button/components/button/button.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
-import { isNil } from '@bimeister/utilities';
+import { isNil, Nullable } from '@bimeister/utilities';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ComponentChange } from '../../../../../internal/declarations/interfaces/component-change.interface';
 import { ComponentChanges } from '../../../../../internal/declarations/interfaces/component-changes.interface';
+import { ButtonIconPosition } from '../../../../../internal/declarations/types/button-icon-position.type';
 import { ButtonKind } from '../../../../../internal/declarations/types/button-kind.type';
 import { ButtonSize } from '../../../../../internal/declarations/types/button-size.type';
 import { ButtonType } from '../../../../../internal/declarations/types/button-type.type';
@@ -28,10 +29,33 @@ export class ButtonComponent {
   @Input() public readonly disabled: boolean = false;
   public readonly disabled$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
+  @Input() public readonly icon: Nullable<string>;
+  public readonly icon$: BehaviorSubject<Nullable<string>> = new BehaviorSubject<Nullable<string>>(null);
+
+  @Input() public readonly iconPosition: ButtonIconPosition = 'left';
+  public readonly iconPosition$: BehaviorSubject<ButtonIconPosition> = new BehaviorSubject<ButtonIconPosition>('left');
+
+  @Input() public readonly loading: boolean;
+  public readonly loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  private readonly iconContainerClass$: Observable<Nullable<string>> = combineLatest([
+    this.icon$,
+    this.iconPosition$
+  ]).pipe(
+    map(([icon, iconPosition]: [Nullable<string>, Nullable<ButtonIconPosition>]) => {
+      if (isNil(icon) || isNil(iconPosition)) {
+        return null;
+      }
+
+      return `with-icon-${iconPosition}`;
+    })
+  );
+
   public readonly resultClassList$: Observable<string[]> = combineLatest([
     this.size$,
     this.kind$,
-    this.disabled$.pipe(map((isDisabled: boolean) => (isDisabled ? 'disabled' : null)))
+    this.disabled$.pipe(map((isDisabled: boolean) => (isDisabled ? 'disabled' : null))),
+    this.iconContainerClass$
   ]).pipe(
     map((classes: string[]) =>
       classes
@@ -45,6 +69,17 @@ export class ButtonComponent {
     this.processTypeChange(changes?.type);
     this.processKindChange(changes?.kind);
     this.processDisabledChange(changes?.disabled);
+    this.processIconChange(changes?.icon);
+    this.processIconPositionChange(changes?.iconPosition);
+    this.processLoadingChange(changes?.loading);
+  }
+
+  public isLeftIconPosition(iconPosition: Nullable<ButtonIconPosition>): boolean {
+    return !isNil(iconPosition) && iconPosition === 'left';
+  }
+
+  public isRightIconPosition(iconPosition: Nullable<ButtonIconPosition>): boolean {
+    return !isNil(iconPosition) && iconPosition === 'right';
   }
 
   private processSizeChange(change: ComponentChange<this, ButtonSize>): void {
@@ -85,5 +120,35 @@ export class ButtonComponent {
     }
 
     this.disabled$.next(updatedValue);
+  }
+
+  private processIconChange(change: ComponentChange<this, string>): void {
+    const updatedValue: string | undefined = change?.currentValue;
+
+    if (typeof updatedValue === 'undefined') {
+      return;
+    }
+
+    this.icon$.next(updatedValue);
+  }
+
+  private processIconPositionChange(change: ComponentChange<this, ButtonIconPosition>): void {
+    const updatedValue: ButtonIconPosition | undefined = change?.currentValue;
+
+    if (typeof updatedValue === 'undefined') {
+      return;
+    }
+
+    this.iconPosition$.next(updatedValue);
+  }
+
+  private processLoadingChange(change: ComponentChange<this, boolean>): void {
+    const updatedValue: boolean | undefined = change?.currentValue;
+
+    if (isNil(updatedValue)) {
+      return;
+    }
+
+    this.loading$.next(updatedValue);
   }
 }
