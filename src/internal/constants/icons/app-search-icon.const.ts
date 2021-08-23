7c729e53474d0d3b2b8e7a4f04c16914a9abd00a
edikdeisling <edikdeisling@gmail.com>
2021-08-23 17:13:55 +0300

    feature: BIM-15209 nav icons

---
+++ src/internal/constants/icons/app-search-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../declarations/interfaces/icon-definition.interface';
+
+export const appSearchIcon: IconDefinition = {
+  name: 'app-search',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.6667 14.6667L11.676 11.6707L14.6667 14.6667ZM13.3333 7.66667C13.3333 9.16956 12.7363 10.6109 11.6736 11.6736C10.6109 12.7363 9.16956 13.3333 7.66667 13.3333C6.16377 13.3333 4.72243 12.7363 3.65973 11.6736C2.59702 10.6109 2 9.16956 2 7.66667C2 6.16377 2.59702 4.72243 3.65973 3.65973C4.72243 2.59702 6.16377 2 7.66667 2C9.16956 2 10.6109 2.59702 11.6736 3.65973C12.7363 4.72243 13.3333 6.16377 13.3333 7.66667V7.66667Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`
+};
