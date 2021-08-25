0ed9f5142549b8ff541fbc7a27317babbf38eed9
Mikhail Kirichenko <mikhail.kirichenko@bimeister.com>
2021-08-25 14:08:20 +0300

    feature: BIM-14890 add copy to clipboard in code block

--- src/lib/components/code/code.module.ts
+++ src/lib/components/code/code.module.ts
@@ -4,11 +4,14 @@ import { CodeComponent } from './components/code/code.component';
 import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 import { CodeContainerComponent } from './components/code-container/code-container.component';
 import { TabsModule } from '../tabs/tabs.module';
+import { ButtonModule } from '../button/button.module';
+import { IconModule } from '../icon/icon.module';
+import { iosCopyIcon } from '../../../internal/constants/icons/ios-copy-icon.const';
 
 @NgModule({
   declarations: [CodeComponent, CodeContainerComponent],
   exports: [CodeComponent, CodeContainerComponent],
-  imports: [SharedModule, TabsModule, HighlightModule],
+  imports: [SharedModule, TabsModule, ButtonModule, IconModule.forFeature([iosCopyIcon]), HighlightModule],
   providers: [
     {
       provide: HIGHLIGHT_OPTIONS,
