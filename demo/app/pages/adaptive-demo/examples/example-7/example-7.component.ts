18a7fbc22f75950c6b43ba2e4d87a48c7dc76adb
Mikhail Kirichenko <mikhail.kirichenko@bimeister.com>
2021-08-26 11:27:20 +0300

    feature: BIM-14890 add examples in adaptive demo

---
+++ demo/app/pages/adaptive-demo/examples/example-7/example-7.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'demo-adaptive-example-7',
+  templateUrl: './example-7.component.html',
+  styleUrls: ['./example-7.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AdaptiveExample7Component {}
---
+++ demo/app/pages/adaptive-demo/examples/example-9/example-9.component.html
@@ -0,0 +1,2 @@
+<div class="block1"></div>
+<div class="block2"></div>
