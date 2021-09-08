5448a462aab5141c64eb7c129edbdffa971d798e
Fedor Usakov <fedor.usakov@bimeister.com>
2021-09-08 12:07:24 +0300

    build: remove unused scripts

--- .storybook/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stories: ['./../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions/register']
-};
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,12 +1,10 @@
 import { parallel, series, task } from 'gulp';
 import { buildDemoStatic } from './build/gulp/build-demo-static.task';
-import { buildIconsTask } from './build/gulp/build-icons.task';
 import { buildLibraryTask } from './build/gulp/build-library.task';
 import { bundleAssetsTask } from './build/gulp/bundle-assets.task';
 import { bundleStyleFilesTask } from './build/gulp/bundle-style-files.task';
-import { createAdaptiveVariablesTask } from './build/gulp/create-adaptive-variables.task';
 import { createBarrelFilesTask } from './build/gulp/create-barrel-files.task';
-import { createFilesTask } from './build/gulp/create-files.task';
+import { createUntrackedFilesTask } from './build/gulp/create-untracked-files.task';
 import { fixFancyLogTask } from './build/gulp/fix-fancy-log.task';
 import { fixImportsTask } from './build/gulp/fix-imports.task';
 
@@ -20,11 +18,7 @@ task(
   )
 );
 
-task('create:files', createFilesTask());
-task('create:barrel-files', createBarrelFilesTask());
-task('create:adaptive-variables', createAdaptiveVariablesTask());
-
-task('convert-icons-to-code', buildIconsTask());
+task('create:untracked-files', createUntrackedFilesTask());
 
 task('build-demo-static', buildDemoStatic());
 
