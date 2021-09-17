778966bf3ab89c472570fbf953001c4d862c0c40
vsevolod.zolotov <vsevolod.zolotov@bimeister.com>
2021-09-17 13:25:06 +0300

    refactor: remove BehabiorSubjects

--- src/lib/components/tag/components/tag/tag.component.ts
+++ src/lib/components/tag/components/tag/tag.component.ts
@@ -3,15 +3,11 @@ import {
   ViewEncapsulation,
   ChangeDetectionStrategy,
   Input,
-  OnChanges,
   ContentChild,
   AfterContentInit
 } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { isNil } from '@bimeister/utilities';
 
-import { ComponentChange } from '../../../../../internal/declarations/interfaces/component-change.interface';
-import { ComponentChanges } from '../../../../../internal/declarations/interfaces/component-changes.interface';
 import { AvatarComponent } from '../../../avatar/components/avatar/avatar.component';
 
 @Component({
@@ -21,21 +17,14 @@ import { AvatarComponent } from '../../../avatar/components/avatar/avatar.compon
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TagComponent implements OnChanges, AfterContentInit {
+export class TagComponent implements AfterContentInit {
   @ContentChild(AvatarComponent) private readonly avatarComponent: AvatarComponent;
 
   @Input() public disabled: boolean = false;
-  public readonly disabled$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   @Input() public clickable: boolean = false;
-  public readonly clickable$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   public readonly withAvatar$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-
-  public ngOnChanges(changes: ComponentChanges<this>): void {
-    this.processDisabledChange(changes?.disabled);
-    this.processClickableChange(changes?.clickable);
-  }
 
   public ngAfterContentInit(): void {
     this.processWithAvatar();
@@ -45,25 +34,5 @@ export class TagComponent implements OnChanges, AfterContentInit {
     if (this.avatarComponent !== undefined) {
       this.withAvatar$.next(true);
     }
-  }
-
-  private processDisabledChange(change: ComponentChange<this, boolean>): void {
-    const updatedValue: boolean | undefined = change?.currentValue;
-
-    if (isNil(updatedValue)) {
-      return;
-    }
-
-    this.disabled$.next(updatedValue);
-  }
-
-  private processClickableChange(change: ComponentChange<this, boolean>): void {
-    const updatedValue: boolean | undefined = change?.currentValue;
-
-    if (isNil(updatedValue)) {
-      return;
-    }
-
-    this.clickable$.next(updatedValue);
   }
 }
