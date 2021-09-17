f9c2e74a4cae41810b8f293918a9923cd11f5d36
vsezol <vsezold@gmail.com>
2021-09-17 11:22:26 +0300

    feature: padding-left: 6px if avatar is in content

--- src/lib/components/tag/components/tag/tag.component.ts
+++ src/lib/components/tag/components/tag/tag.component.ts
@@ -1,9 +1,18 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input, OnChanges } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+  Input,
+  OnChanges,
+  ContentChild,
+  AfterContentInit
+} from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { isNil } from '@bimeister/utilities';
 
 import { ComponentChange } from '../../../../../internal/declarations/interfaces/component-change.interface';
 import { ComponentChanges } from '../../../../../internal/declarations/interfaces/component-changes.interface';
+import { AvatarComponent } from '../../../avatar/components/avatar/avatar.component';
 
 @Component({
   selector: 'pupa-tag',
@@ -12,16 +21,30 @@ import { ComponentChanges } from '../../../../../internal/declarations/interface
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TagComponent implements OnChanges {
+export class TagComponent implements OnChanges, AfterContentInit {
+  @ContentChild(AvatarComponent) private readonly avatarComponent: AvatarComponent;
+
   @Input() public disabled: boolean = false;
   public readonly disabled$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   @Input() public clickable: boolean = false;
   public readonly clickable$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
+  public readonly withAvatar$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
   public ngOnChanges(changes: ComponentChanges<this>): void {
     this.processDisabledChange(changes?.disabled);
     this.processClickableChange(changes?.clickable);
+  }
+
+  public ngAfterContentInit(): void {
+    this.processWithAvatar();
+  }
+
+  private processWithAvatar(): void {
+    if (this.avatarComponent !== undefined) {
+      this.withAvatar$.next(true);
+    }
   }
 
   private processDisabledChange(change: ComponentChange<this, boolean>): void {
