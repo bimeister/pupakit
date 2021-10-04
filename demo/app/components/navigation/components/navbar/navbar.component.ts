27686663638c24d0f0113592a18bd8907c8cfbf4
vsezol <vsezold@gmail.com>
2021-10-04 17:58:25 +0300

    feature: add open/close drawer functionality

--- demo/app/components/navigation/components/navbar/navbar.component.ts
+++ demo/app/components/navigation/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ChangeDetectionStrategy, Component, Injector, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, of, BehaviorSubject, Subscription } from 'rxjs';
+import { filter, map, switchMap, take } from 'rxjs/operators';
+import { filterTruthy } from '@bimeister/utilities';
 
 import { ThemeWrapperService } from '../../../../../../src/lib/components/theme-wrapper/services/theme-wrapper.service';
 import { Theme } from '../../../../../../src/internal/declarations/enums/theme.enum';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { FormControl } from '@angular/forms';
+import { DrawersService } from '../../../../../../src/internal/shared/services/drawers.service';
+import { DRAWER_DATA_TOKEN } from '../../../../../declarations/tokens/drawer-data.token';
+import { MenuDrawerComponent } from '../menu-drawer/menu-drawer.component';
+import { OpenedDrawer } from '../../../../../../src/internal/declarations/interfaces/opened-drawer.interface';
 
 @Component({
   selector: 'demo-navbar',
@@ -14,24 +19,103 @@ import { FormControl } from '@angular/forms';
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavbarComponent {
-  private readonly logoDark: SafeResourceUrl;
-  private readonly logoLight: SafeResourceUrl;
-
-  public isNavbarOpen: FormControl = new FormControl(false);
-
+export class NavbarComponent implements OnDestroy {
   public readonly logo$: Observable<SafeResourceUrl> = this.themeWrapperService.theme$.pipe(
     map((themeMode: Theme) => {
       return themeMode === Theme.Light ? this.logoLight : this.logoDark;
     })
   );
 
-  constructor(private readonly themeWrapperService: ThemeWrapperService, private readonly sanitizer: DomSanitizer) {
+  private readonly logoDark: SafeResourceUrl;
+  private readonly logoLight: SafeResourceUrl;
+
+  private readonly subscription: Subscription = new Subscription();
+
+  private readonly openedDrawerInitialValue: OpenedDrawer = {
+    id: null,
+    closed$: of(null)
+  };
+
+  public isMenuOpen: FormControl = new FormControl(false);
+
+  private readonly menuOpen$: Observable<true> = this.isMenuOpen.valueChanges.pipe(filterTruthy());
+
+  private readonly openedDrawer$: BehaviorSubject<OpenedDrawer> = new BehaviorSubject<OpenedDrawer>(
+    this.openedDrawerInitialValue
+  );
+
+  private readonly drawerOpened$: Observable<OpenedDrawer> = this.openedDrawer$.pipe(
+    filter((openDrawer: OpenedDrawer) => typeof openDrawer.id === 'string')
+  );
+
+  private readonly drawerClosed$: Observable<null> = this.drawerOpened$.pipe(
+    switchMap((openDrawer: OpenedDrawer) => openDrawer.closed$)
+  );
+
+  constructor(
+    private readonly themeWrapperService: ThemeWrapperService,
+    private readonly sanitizer: DomSanitizer,
+    private readonly drawerService: DrawersService,
+    private readonly injector: Injector
+  ) {
     this.logoLight = this.sanitizer.bypassSecurityTrustResourceUrl('assets/logo-light.svg');
     this.logoDark = this.sanitizer.bypassSecurityTrustResourceUrl('assets/logo-dark.svg');
+
+    this.subscription.add(this.closeMenuWhenDrawerClosed());
+    this.subscription.add(this.openDrawerWhenMenuOpen());
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   public handleToggleNavbar(isOpen: boolean): void {
-    this.isNavbarOpen.setValue(isOpen);
+    this.isMenuOpen.setValue(isOpen);
+
+    if (!isOpen) {
+      this.closeDrawer();
+    }
+  }
+
+  private closeMenuWhenDrawerClosed(): Subscription {
+    return this.drawerClosed$.subscribe(() => {
+      this.openedDrawer$.next({ ...this.openedDrawerInitialValue });
+      this.isMenuOpen.setValue(false);
+    });
+  }
+
+  private openDrawerWhenMenuOpen(): Subscription {
+    return this.menuOpen$.subscribe(() => {
+      const openedDrawer: OpenedDrawer = this.openDrawer();
+      this.openedDrawer$.next(openedDrawer);
+    });
+  }
+
+  private openDrawer(): OpenedDrawer {
+    return this.drawerService.open(MenuDrawerComponent, {
+      hasBackdrop: true,
+      closeOnBackdropClick: true,
+      isBackdropTransparent: false,
+      float: 'right',
+      injector: this.injector,
+      providers: [
+        {
+          provide: DRAWER_DATA_TOKEN,
+          useValue: [1, 2, 3, 4]
+        }
+      ]
+    });
+  }
+
+  private closeDrawer(): void {
+    this.drawerOpened$
+      .pipe(
+        map((openDrawer: OpenedDrawer) => openDrawer.id),
+        take(1)
+      )
+      .subscribe((id: string) => {
+        this.drawerService.closeById(id);
+        this.openedDrawer$.next({ ...this.openedDrawerInitialValue });
+      });
   }
 }
