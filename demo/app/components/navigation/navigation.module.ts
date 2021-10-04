c1150476eb7308433b781fdfd6e4c3ce9b5bf709
vsevolod.zolotov <vsevolod.zolotov@bimeister.com>
2021-10-04 17:58:25 +0300

    feature: add open/close drawer functionality

--- demo/app/components/navigation/navigation.module.ts
+++ demo/app/components/navigation/navigation.module.ts
@@ -3,9 +3,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
 import { DemoSharedModule } from '../../shared/shared.module';
 import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
+import { MenuDrawerComponent } from './components/menu-drawer/menu-drawer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, ThemeToggleComponent, MenuToggleComponent],
+  declarations: [NavbarComponent, ThemeToggleComponent, MenuToggleComponent, MenuDrawerComponent],
   exports: [NavbarComponent],
   imports: [DemoSharedModule]
 })
