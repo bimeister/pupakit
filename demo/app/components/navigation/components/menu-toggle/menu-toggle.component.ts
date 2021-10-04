1832de425dac0ce07292de91206938c6fd19fde0
vsezol <vsezold@gmail.com>
2021-10-04 16:09:21 +0300

    feature: menu-toggle.component

---
+++ demo/app/components/navigation/components/menu-toggle/menu-toggle.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'demo-menu-toggle',
+  templateUrl: './menu-toggle.component.html',
+  styleUrls: ['./menu-toggle.component.scss']
+})
+export class MenuToggleComponent {
+  @Input() public isOpen: boolean = false;
+
+  @Output() public readonly toggle: EventEmitter<boolean> = new EventEmitter();
+
+  public handleToggle(): void {
+    this.toggle.emit(!this.isOpen);
+  }
+}
--- demo/app/components/navigation/navigation.module.ts
+++ demo/app/components/navigation/navigation.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
 import { DemoSharedModule } from '../../shared/shared.module';
+import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
 
 @NgModule({
-  declarations: [NavbarComponent, ThemeToggleComponent],
+  declarations: [NavbarComponent, ThemeToggleComponent, MenuToggleComponent],
   exports: [NavbarComponent],
   imports: [DemoSharedModule]
 })
