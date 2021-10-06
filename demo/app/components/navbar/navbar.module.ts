efbc64331c1f47273c3dc2edfdf765e672d1e3cd
vsezol <vsezold@gmail.com>
2021-10-06 17:49:30 +0300

    feature: theme saving

--- demo/app/components/navbar/navbar.module.ts
+++ demo/app/components/navbar/navbar.module.ts
@@ -4,10 +4,12 @@ import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.com
 import { DemoSharedModule } from '../../shared/shared.module';
 import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
 import { GridToggleComponent } from './components/grid-toggle/grid-toggle.component';
+import { ThemeSaverService } from './theme-saver.service';
 
 @NgModule({
   declarations: [NavbarComponent, ThemeToggleComponent, MenuToggleComponent, GridToggleComponent],
   exports: [NavbarComponent],
-  imports: [DemoSharedModule]
+  imports: [DemoSharedModule],
+  providers: [ThemeSaverService]
 })
 export class NavbarModule {}
---
+++ demo/app/components/navbar/theme-saver.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { Theme } from '../../../../src/internal/declarations/enums/theme.enum';
+import { isNil } from '@bimeister/utilities';
+
+const THEME_MODE_KEY: string = 'theme-mode';
+
+@Injectable()
+export class ThemeSaverService {
+  public getTheme(): Theme {
+    const item: string | null = localStorage.getItem(THEME_MODE_KEY);
+
+    if (!isNil(item)) {
+      return item as Theme;
+    }
+
+    return Theme.Light;
+  }
+
+  public setTheme(theme: Theme): void {
+    localStorage.setItem(THEME_MODE_KEY, theme);
+  }
+}
