e91a4bbdc637cf8a96bce59702b890f1b199c5ec
vsezol <vsezold@gmail.com>
2021-10-07 14:09:34 +0300

    feature: provide theme for dropdown, use demo-props-radio-select instead of pupa-radio-control

--- demo/app/pages/dropdown-menu-demo/dropdown-menu-demo.component.ts
+++ demo/app/pages/dropdown-menu-demo/dropdown-menu-demo.component.ts
@@ -1,6 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Theme } from '../../../../src/internal/declarations/enums/theme.enum';
+import { ThemeWrapperService } from '../../../../src/lib/components/theme-wrapper/services/theme-wrapper.service';
+import { Observable } from 'rxjs';
+import { RadioOption } from '../../shared/components/example-viewer/radio-option';
 
 @Component({
   selector: 'demo-dropdown-menu',
@@ -10,7 +13,24 @@ import { Theme } from '../../../../src/internal/declarations/enums/theme.enum';
 })
 export class DropdownMenuDemoComponent {
   public readonly theme: typeof Theme = Theme;
-  public readonly themeControl: FormControl = new FormControl(Theme.Light);
   public readonly triggerText: FormControl = new FormControl('Show dropdown');
-  public readonly horizontalPosition: FormControl = new FormControl('center');
+
+  public readonly horizontalPositionOptions: RadioOption[] = [
+    {
+      caption: 'start',
+      value: 'start'
+    },
+    {
+      caption: 'center',
+      value: 'center'
+    },
+    {
+      caption: 'end',
+      value: 'end'
+    }
+  ];
+
+  public readonly theme$: Observable<Theme> = this.themeWrapperService.theme$;
+
+  constructor(private readonly themeWrapperService: ThemeWrapperService) {}
 }
