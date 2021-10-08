b3580fb34ca3e7737c62d42ca9e01316f5ad9fe6
ScarletFlash <scarletflash.dev@gmail.com>
2021-10-08 14:37:58 +0000

    fix: BIM-15810: Checkbox indeterminate icons

--- src/lib/components/checkbox/checkbox.module.ts
+++ src/lib/components/checkbox/checkbox.module.ts
@@ -1,17 +1,17 @@
+import { appCheckboxMinusIcon } from '../../../internal/constants/icons/app-checkbox-minus-icon.const';
+import { appCheckboxCheckIcon } from '../../../internal/constants/icons/app-checkbox-check-icon';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './../../../internal/shared/shared.module';
 import { CheckboxMarkComponent } from './components/checkbox-mark/checkbox-mark.component';
 import { IconModule } from '../icon/icon.module';
-import { iosCheckmarkIcon } from '../../../internal/constants/icons/ios-checkmark-icon.const';
-import { iosSquareIcon } from '../../../internal/constants/icons/ios-square-icon.const';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { CheckboxLabelComponent } from './components/checkbox-label/checkbox-label.component';
 import { CheckboxHintComponent } from './components/checkbox-hint/checkbox-hint.component';
 
 @NgModule({
   declarations: [CheckboxMarkComponent, CheckboxComponent, CheckboxLabelComponent, CheckboxHintComponent],
-  imports: [SharedModule, IconModule.forFeature([iosCheckmarkIcon, iosSquareIcon])],
+  imports: [SharedModule, IconModule.forFeature([appCheckboxCheckIcon, appCheckboxMinusIcon])],
   exports: [CheckboxMarkComponent, CheckboxComponent, CheckboxLabelComponent, CheckboxHintComponent]
 })
 export class CheckboxModule {}
