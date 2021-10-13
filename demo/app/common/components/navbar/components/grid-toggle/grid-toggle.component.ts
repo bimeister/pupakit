8f2e6bd4724eaca63949ddd6a61cdceaabfb5686
vsevolod.zolotov <vsevolod.zolotov@bimeister.com>
2021-10-13 18:20:54 +0300

    fix: apply suggested fixes

--- demo/app/common/components/navbar/components/grid-toggle/grid-toggle.component.ts
+++ demo/app/common/components/navbar/components/grid-toggle/grid-toggle.component.ts
@@ -1,6 +1,4 @@
-import { Component, Input, OnDestroy } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Component, Input } from '@angular/core';
 import { GridStateService } from '../../../../../../../src/internal/shared/services/grid-state.service';
 
 @Component({
@@ -8,21 +6,13 @@ import { GridStateService } from '../../../../../../../src/internal/shared/servi
   templateUrl: './grid-toggle.component.html',
   styleUrls: ['./grid-toggle.component.scss']
 })
-export class GridToggleComponent implements OnDestroy {
+export class GridToggleComponent {
   @Input() public label: string = '';
-  public readonly gridVisibleFormControl: FormControl = new FormControl(false);
+  public readonly isGridVisible: boolean = false;
 
-  private readonly subscription: Subscription = new Subscription();
+  constructor(private readonly gridStateService: GridStateService) {}
 
-  constructor(private readonly gridStateService: GridStateService) {
-    this.subscription.add(this.processGridVisibleStateControlChanges());
-  }
-
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  private processGridVisibleStateControlChanges(): Subscription {
-    return this.gridVisibleFormControl.valueChanges.subscribe(() => this.gridStateService.toggleGridVisibleState());
+  public toggleGridVisible(): void {
+    this.gridStateService.toggleGridVisibleState();
   }
 }
