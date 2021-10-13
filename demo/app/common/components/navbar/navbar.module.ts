4c0ebe1063de0c25735416096814bcaaa0a06788
vsezol <vsezold@gmail.com>
2021-10-13 18:20:54 +0300

    fix: apply suggested fixes

--- demo/app/common/components/navbar/navbar.module.ts
+++ demo/app/common/components/navbar/navbar.module.ts
@@ -5,10 +5,11 @@ import { DemoSharedModule } from '../../../shared/shared.module';
 import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
 import { GridToggleComponent } from './components/grid-toggle/grid-toggle.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NavbarComponent, ThemeToggleComponent, MenuToggleComponent, GridToggleComponent],
   exports: [NavbarComponent],
-  imports: [DemoSharedModule, RouterModule]
+  imports: [DemoSharedModule, RouterModule, FormsModule]
 })
 export class NavbarModule {}
