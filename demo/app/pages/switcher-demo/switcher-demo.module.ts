f8d099545594ad5580ebd60f654d450376dc42c1
Mikhail Kirichenko <mikhail.kirichenko@bimeister.com>
2021-10-18 18:27:41 +0300

    feature: BIM-11598 switcher component

--- demo/app/pages/switcher-demo/switcher-demo.module.ts
+++ demo/app/pages/switcher-demo/switcher-demo.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
 import { DemoSharedModule } from '../../shared/shared.module';
+import { SwitcherExample1Component } from './examples/example-1/example-1.component';
 import { SwitcherDemoRoutingModule } from './switcher-demo-routing.module';
 import { SwitcherDemoComponent } from './switcher-demo.component';
 
+const EXAMPLES: Type<unknown>[] = [SwitcherExample1Component];
+const COMPONENTS: Type<unknown>[] = [SwitcherDemoComponent, ...EXAMPLES];
+
+const DECLARATIONS: Type<unknown>[] = [...COMPONENTS];
+
 @NgModule({
-  declarations: [SwitcherDemoComponent],
+  declarations: [...DECLARATIONS],
   imports: [DemoSharedModule, SwitcherDemoRoutingModule]
 })
 export class SwitcherDemoModule {}
--- src/lib/components/switcher/components/switcher/switcher.component.ts
+++ src/lib/components/switcher/components/switcher/switcher.component.ts
@@ -1,80 +1,133 @@
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   EventEmitter,
   forwardRef,
   Input,
-  Output
+  Output,
+  ViewEncapsulation
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { filterFalsy, filterTruthy, isNil, Nullable } from '@bimeister/utilities';
+import { BehaviorSubject } from 'rxjs';
+import { map, switchMapTo, take } from 'rxjs/operators';
+import { ComponentChange } from '../../../../../internal/declarations/interfaces/component-change.interface';
+import { ComponentChanges } from '../../../../../internal/declarations/interfaces/component-changes.interface';
+import { SwitcherSize } from '../../../../../internal/declarations/types/switcher-size.type';
 
 @Component({
   selector: 'pupa-switcher',
   templateUrl: './switcher.component.html',
   styleUrls: ['./switcher.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => SwitcherComponent),
       multi: true
     }
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  ]
 })
 export class SwitcherComponent implements ControlValueAccessor {
-  @Input() public id: string;
-  @Input() public disabled: boolean;
-  @Output() public change: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() public disabled: boolean = false;
+  @Input() public value: boolean;
+  @Input() public tabindex: number = 0;
 
-  private _value: boolean = false;
-  get value(): boolean {
-    return this._value;
+  @Input() public size: SwitcherSize = 'medium';
+
+  public readonly value$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public readonly isDisabled$: BehaviorSubject<Nullable<boolean>> = new BehaviorSubject<boolean>(null);
+  public readonly isTouched$: BehaviorSubject<Nullable<boolean>> = new BehaviorSubject<boolean>(null);
+  public readonly isFocused$: BehaviorSubject<Nullable<boolean>> = new BehaviorSubject<boolean>(null);
+
+  @Output() public focus: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
+  @Output() public blur: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
+
+  @Output() public updateValue: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  public ngOnChanges(changes: ComponentChanges<this>): void {
+    this.handleDisabledChanges(changes?.disabled);
+    this.handleValueChanges(changes?.value);
   }
 
-  @Input() set value(v: boolean) {
-    if (v !== this._value) {
-      this._value = v;
-      this.onChange(v);
-      this.change.emit(v);
-    }
+  public get sizeClass(): string {
+    return `switcher_${this.size}`;
   }
 
-  constructor(private readonly changeDetectorRef: ChangeDetectorRef) {}
+  public onKeyDown(_event: MouseEvent): void {
+    this.isFocused$.pipe(take(1), filterTruthy()).subscribe(() => this.changeValue());
+  }
 
-  public onChange: (v: boolean) => void = (v: boolean) => {
-    this.change.emit(v);
-  };
-  public onTouched: () => void = () => undefined;
+  public changeValue(): void {
+    this.isDisabled$
+      .pipe(
+        take(1),
+        filterFalsy(),
+        switchMapTo(this.value$),
+        take(1),
+        map((value: boolean) => !value)
+      )
+      .subscribe((value: boolean) => {
+        this.value$.next(value);
+        this.onTouched();
+        this.onChange(value);
+
+        this.updateValue.emit(value);
+      });
+  }
+
+  public registerOnChange(onChangeCallback: VoidFunction): void {
+    this.onChange = onChangeCallback;
+  }
+
+  public registerOnTouched(onTouchedCallback: VoidFunction): void {
+    this.onTouched = onTouchedCallback;
+  }
 
   public writeValue(value: boolean): void {
-    if ((value as any) instanceof Event) {
-      return;
-    }
-    this.changeDetectorRef.markForCheck();
-    this._value = value;
-  }
-
-  public toggle(): void {
-    if (this.disabled) {
-      return;
-    }
-    this.value = !this.value;
-  }
-
-  public registerOnChange(fn: (v: boolean) => void): void {
-    this.onChange = (v: boolean): void => {
-      this.change.emit(v);
-      fn(v);
-    };
-  }
-
-  public registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
+    this.value$.next(value);
   }
 
   public setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-    this.changeDetectorRef.markForCheck();
+    this.isDisabled$.next(isDisabled);
+  }
+
+  public emitFocusEvent(focusEvent: FocusEvent): void {
+    this.isFocused$.next(true);
+    this.focus.emit(focusEvent);
+  }
+
+  public emitBlurEvent(blurEvent: FocusEvent): void {
+    this.isFocused$.next(false);
+    this.blur.emit(blurEvent);
+  }
+
+  public onChange: CallableFunction = (_innerValue: string) => {
+    return;
+  };
+
+  public onTouched: VoidFunction = () => {
+    return;
+  };
+
+  private handleValueChanges(change: ComponentChange<this, boolean>): void {
+    const updatedValue: Nullable<boolean> = change?.currentValue;
+
+    if (isNil(updatedValue)) {
+      return;
+    }
+
+    this.writeValue(updatedValue);
+  }
+
+  private handleDisabledChanges(change: ComponentChange<this, boolean>): void {
+    const updatedValue: Nullable<boolean> = change?.currentValue;
+
+    if (isNil(updatedValue)) {
+      return;
+    }
+
+    this.setDisabledState(updatedValue);
   }
 }
