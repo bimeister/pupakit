b2599bd3dffc32cb94d3d0325fb75146b3fa23f2
progikusok <moyomylo11@gmail.com>
2021-10-19 13:41:26 +0300

    chore: add demo pupa-code-inline component; remove switcher disabled attribute

---
+++ demo/app/shared/components/code/components/code-inline/code-inline.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'pupa-code-inline',
+  templateUrl: './code-inline.component.html',
+  styleUrls: ['./code-inline.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CodeInlineComponent {}
