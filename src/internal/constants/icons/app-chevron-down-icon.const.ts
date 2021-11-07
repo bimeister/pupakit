404539f3ee81eb74dec0de255c6edcaf51cab61d
Vladilen Zamyshlyaev <vladilen.zamyshlyaev@bimeister.com>
2021-11-07 17:55:24 +0300

    feature: BIM-16266 add chevron icons

---
+++ src/internal/constants/icons/app-chevron-down-icon.const.ts
@@ -0,0 +1,8 @@
+import { IconDefinition } from '../../declarations/interfaces/icon-definition.interface';
+
+export const appChevronDownIcon: IconDefinition = {
+  name: 'app-chevron-down',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12 6L8.00002 10L4.00009 6.00001" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`
+};
