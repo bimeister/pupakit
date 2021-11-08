4f538b5bb709f4e7e9241334ac7da2b12a570f86
gaevivan <6507085-gaevivan@users.noreply.gitlab.com>
2021-11-08 13:22:40 +0300

    chore: BIM-16264 code review

--- src/internal/pipes/pipes.module.ts
+++ src/internal/pipes/pipes.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { ConditionAsyncPipe } from './condition-async.pipe';
 import { FormatCountPipe } from './format-count.pipe';
 
 import { IsNullOrUndefinedPipe } from './is-null-or-undefined.pipe';
 import { TimeFormatPipe } from './time-format.pipe';
 import { ToString } from './to-string.pipe';
 
-const SHARED_PIPES: any[] = [IsNullOrUndefinedPipe, ToString, TimeFormatPipe, FormatCountPipe];
+const SHARED_PIPES: any[] = [IsNullOrUndefinedPipe, ToString, TimeFormatPipe, FormatCountPipe, ConditionAsyncPipe];
 
 @NgModule({
   declarations: [...SHARED_PIPES],
