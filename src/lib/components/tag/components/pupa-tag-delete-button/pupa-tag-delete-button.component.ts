e3032d4802de08651c2b3ba94efc8f39ed0f5fbf
Georgy <georgy.melomordanov@bimeister.com>
2021-11-23 11:52:18 +0300

    feature: BIM-16979 Доработать функционал тегов (pupa-tag)

---
+++ src/lib/components/tag/components/pupa-tag-delete-button/pupa-tag-delete-button.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'pupa-pupa-tag-delete-button',
+  templateUrl: './pupa-tag-delete-button.component.html',
+  styleUrls: ['./pupa-tag-delete-button.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TagDeleteButtonComponent {
+  @Output() public readonly close: EventEmitter<void> = new EventEmitter();
+
+  public closeHandler(): void {
+    this.close.emit();
+  }
+}
--- src/lib/components/tag/tag.module.ts
+++ src/lib/components/tag/tag.module.ts
@@ -1,16 +1,17 @@
 import { NgModule, Type } from '@angular/core';
 import { SharedModule } from '../../../internal/shared/shared.module';
+import { IconModule } from '../icon/icon.module';
+import { TagDeleteButtonComponent } from './components/pupa-tag-delete-button/pupa-tag-delete-button.component';
 import { TagComponent } from './components/tag/tag.component';
-import { ColorTagComponent } from './components/color-tag/color-tag.component';
 
-const COMPONENTS: Type<unknown>[] = [TagComponent, ColorTagComponent];
+const COMPONENTS: Type<unknown>[] = [TagComponent, TagDeleteButtonComponent];
 
 const DECLARATIONS: Type<unknown>[] = [...COMPONENTS];
 const EXPORTS: Type<unknown>[] = [...DECLARATIONS];
 
 @NgModule({
   declarations: [...DECLARATIONS],
-  imports: [SharedModule],
+  imports: [SharedModule, IconModule],
   exports: [...EXPORTS]
 })
 export class TagModule {}
