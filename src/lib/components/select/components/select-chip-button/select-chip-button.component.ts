8d2950101623c7b6c34eb20515fa89500adee49c
progikusok <moyomylo11@gmail.com>
2021-11-29 15:30:03 +0300

    feature: BIM-17006 select design update

---
+++ src/lib/components/select/components/select-chip-button/select-chip-button.component.ts
@@ -0,0 +1,22 @@
+import { CdkOverlayOrigin } from '@angular/cdk/overlay';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { SelectButtonBase } from '../../../../../internal/declarations/classes/abstract/select-button-base.abstract';
+import { SelectStateService } from '../../services/select-state.service';
+
+@Component({
+  selector: 'pupa-select-chip-button',
+  templateUrl: './select-chip-button.component.html',
+  styleUrls: ['./select-chip-button.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SelectChipButtonComponent<T> extends SelectButtonBase<T> {
+  @ViewChild('overlayOrigin', { static: true }) protected readonly overlayOrigin: CdkOverlayOrigin;
+  @ViewChild('button', { static: true }) protected readonly button: ElementRef<HTMLButtonElement>;
+
+  public transparent: boolean = false;
+
+  constructor(selectStateService: SelectStateService<T>) {
+    super(selectStateService);
+  }
+}
