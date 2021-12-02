0714a540fbe347da3dc2d4e59fc5af43fcc0f9f1
progikusok <moyomylo11@gmail.com>
2021-12-02 16:52:09 +0300

    feature: add example: init

---
+++ demo/app/pages/select-demo/examples/example-1/example-1.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'demo-select-example-1',
+  templateUrl: './example-1.component.html',
+  styleUrls: ['./example-1.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SelectExample1Component {
+  public readonly control: FormControl = new FormControl();
+}
--- src/lib/components/select/components/select-button/select-button.component.ts
+++ src/lib/components/select/components/select-button/select-button.component.ts
@@ -1,6 +1,5 @@
 import { CdkOverlayOrigin } from '@angular/cdk/overlay';
-import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild, ViewEncapsulation } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { SelectStateService } from '../../services/select-state.service';
 import { SelectButtonBase } from './../../../../../internal/declarations/classes/abstract/select-button-base.abstract';
 
@@ -14,8 +13,6 @@ import { SelectButtonBase } from './../../../../../internal/declarations/classes
 export class SelectButtonComponent<T> extends SelectButtonBase<T> {
   @ViewChild('overlayOrigin', { static: true }) protected readonly overlayOrigin: CdkOverlayOrigin;
   @ViewChild('button', { static: true }) protected readonly button: ElementRef<HTMLButtonElement>;
-
-  @Input() public transparent: boolean = false;
 
   constructor(selectStateService: SelectStateService<T>) {
     super(selectStateService);
