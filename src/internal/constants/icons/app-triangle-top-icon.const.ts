8ca3164d23a56f130a9624ced1324dbdbed58d52
gaevivan <ivan.gaev@bimeister.com>
2021-12-07 17:42:15 +0300

    feature: BIM-16794 exemplar-card icons added

---
+++ src/internal/constants/icons/app-triangle-top-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../declarations/interfaces/icon-definition.interface';
+
+export const appTriangleTopIcon: IconDefinition = {
+  name: 'app-triangle-top',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M7.39645 6.35355C7.59171 6.15829 7.90829 6.15829 8.10355 6.35355L10.3964 8.64645C10.7114 8.96143 10.4883 9.5 10.0429 9.5L5.45711 9.5C5.01165 9.5 4.78857 8.96143 5.10355 8.64645L7.39645 6.35355Z" fill="currentColor"/>
+  </svg>
+`,
+};
