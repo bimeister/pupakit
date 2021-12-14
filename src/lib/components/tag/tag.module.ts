11d3565a06f61f61a09a308a7c572fd652240f88
Mikhail Kirichenko <mikhail.kirichenko@bimeister.com>
2021-12-14 17:43:51 +0300

    feature: BIM-17006 pupa-tag icons module support

--- src/lib/components/tag/tag.module.ts
+++ src/lib/components/tag/tag.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, Type } from '@angular/core';
+import { appExceptionsCross8Icon } from '../../../internal/constants/icons/app-exceptions-cross-8-icon.const';
 import { SharedModule } from '../../../internal/shared/shared.module';
 import { IconModule } from '../icon/icon.module';
 import { TagDeleteButtonComponent } from './components/pupa-tag-delete-button/pupa-tag-delete-button.component';
@@ -12,7 +13,7 @@ const EXPORTS: Type<unknown>[] = [...DECLARATIONS];
 
 @NgModule({
   declarations: [...DECLARATIONS],
-  imports: [SharedModule, IconModule],
+  imports: [SharedModule, IconModule.forFeature([appExceptionsCross8Icon])],
   exports: [...EXPORTS],
 })
 export class TagModule {}
