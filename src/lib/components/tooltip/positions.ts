67febbff5c0caf2e2827fc877bf0c51b951fccea
gaevivan <ivan.gaev@bimeister.com>
2021-12-15 17:27:30 +0300

    fix: BIM-17081 tooltip position

--- src/lib/components/tooltip/positions.ts
+++ src/lib/components/tooltip/positions.ts
@@ -1,61 +1,16 @@
 import { ConnectedPosition } from '@angular/cdk/overlay';
 
-const OVERLAY_TOP_POSITION: ConnectedPosition = {
+const CENTER_BOTTOM_POSITION: ConnectedPosition = {
   originX: 'center',
   originY: 'top',
   overlayX: 'center',
   overlayY: 'bottom',
 };
-const OVERLAY_RIGHT_POSITION: ConnectedPosition = {
-  originX: 'end',
-  originY: 'center',
-  overlayX: 'start',
-  overlayY: 'center',
-};
-const OVERLAY_BOTTOM_POSITION: ConnectedPosition = {
+const CENTER_TOP_POSITION: ConnectedPosition = {
   originX: 'center',
   originY: 'bottom',
   overlayX: 'center',
-  overlayY: 'top',
-};
-const OVERLAY_LEFT_POSITION: ConnectedPosition = {
-  originX: 'start',
-  originY: 'center',
-  overlayX: 'end',
-  overlayY: 'center',
-};
-const OVERLAY_LEFT_TOP_POSITION: ConnectedPosition = {
-  originX: 'start',
-  originY: 'top',
-  overlayX: 'end',
-  overlayY: 'bottom',
-};
-const OVERLAY_RIGHT_TOP_POSITION: ConnectedPosition = {
-  originX: 'end',
-  originY: 'top',
-  overlayX: 'start',
-  overlayY: 'bottom',
-};
-const OVERLAY_RIGHT_BOTTOM_POSITION: ConnectedPosition = {
-  originX: 'end',
-  originY: 'bottom',
-  overlayX: 'start',
-  overlayY: 'top',
-};
-const OVERLAY_LEFT_BOTTOM_POSITION: ConnectedPosition = {
-  originX: 'start',
-  originY: 'bottom',
-  overlayX: 'end',
   overlayY: 'top',
 };
 
-export const OVERLAY_POSITIONS: ConnectedPosition[] = [
-  OVERLAY_TOP_POSITION,
-  OVERLAY_RIGHT_POSITION,
-  OVERLAY_BOTTOM_POSITION,
-  OVERLAY_LEFT_POSITION,
-  OVERLAY_LEFT_TOP_POSITION,
-  OVERLAY_RIGHT_TOP_POSITION,
-  OVERLAY_RIGHT_BOTTOM_POSITION,
-  OVERLAY_LEFT_BOTTOM_POSITION,
-];
+export const OVERLAY_POSITIONS: ConnectedPosition[] = [CENTER_BOTTOM_POSITION, CENTER_TOP_POSITION];
