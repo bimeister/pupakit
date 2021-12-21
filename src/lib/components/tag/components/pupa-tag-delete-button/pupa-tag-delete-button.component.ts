d336aa53731be2a0ae7ee12b821702f2612475d3
progikusok <moyomylo11@gmail.com>
2021-12-21 13:14:35 +0300

    feature: tag refactoring

--- src/lib/components/tag/components/pupa-tag-delete-button/pupa-tag-delete-button.component.ts
+++ src/lib/components/tag/components/pupa-tag-delete-button/pupa-tag-delete-button.component.ts
@@ -1,16 +1,24 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { filterTruthy } from '@bimeister/utilities';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { TagStateService } from '../../services/tag-state.service';
 
 @Component({
-  selector: 'pupa-pupa-tag-delete-button',
+  selector: 'pupa-tag-delete-button',
   templateUrl: './pupa-tag-delete-button.component.html',
   styleUrls: ['./pupa-tag-delete-button.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TagDeleteButtonComponent {
-  @Output() public readonly close: EventEmitter<void> = new EventEmitter();
+  public readonly isDisabled$: Observable<boolean> = this.tagStateService.isDisabled$;
 
-  public closeHandler(): void {
-    this.close.emit();
+  constructor(private readonly tagStateService: TagStateService) {}
+
+  public closeHandler(event: Event): void {
+    this.isDisabled$.pipe(take(1), filterTruthy()).subscribe(() => {
+      event.stopPropagation();
+    });
   }
 }
--- src/lib/components/tag/components/tag/tag.component.ts
+++ src/lib/components/tag/components/tag/tag.component.ts
@@ -4,62 +4,63 @@ import {
   Component,
   ContentChild,
   Input,
+  OnChanges,
   ViewEncapsulation,
 } from '@angular/core';
-import { isNil } from '@bimeister/utilities';
+import { isNil, Nullable } from '@bimeister/utilities';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ComponentChange } from '../../../../../internal/declarations/interfaces/component-change.interface';
+import { ComponentChanges } from '../../../../../internal/declarations/interfaces/component-changes.interface';
 import { TagSize } from '../../../../../internal/declarations/types/color-size.type';
 import { TagColor } from '../../../../../internal/declarations/types/color-tag.types';
 import { AvatarComponent } from '../../../avatar/components/avatar/avatar.component';
+import { TagStateService } from '../../services/tag-state.service';
 
 @Component({
   selector: 'pupa-tag',
   templateUrl: './tag.component.html',
   styleUrls: ['./tag.component.scss'],
+  providers: [TagStateService],
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TagComponent implements AfterContentInit {
+export class TagComponent implements AfterContentInit, OnChanges {
   @ContentChild(AvatarComponent) private readonly avatarComponent: AvatarComponent;
 
-  @Input() public set color(value: TagColor) {
-    if (isNil(value)) {
-      return;
-    }
-
-    this.$color.next(value);
-  }
-  private readonly $color: BehaviorSubject<TagColor> = new BehaviorSubject<TagColor>('default');
-
-  public readonly colorClass$: Observable<string> = this.$color.pipe(map((color: TagColor) => `tag_${color}`));
-
-  @Input() public set size(value: TagSize) {
-    if (isNil(value)) {
-      return;
-    }
-
-    this.$size.next(value);
-  }
-  private readonly $size: BehaviorSubject<TagSize> = new BehaviorSubject<TagSize>('medium');
-
-  public readonly sizeClass$: Observable<string> = this.$size.pipe(map((size: TagSize) => `tag_${size}`));
-
   @Input() public disabled: boolean = false;
+  public readonly isDisabled$: BehaviorSubject<boolean> = this.tagStateService.isDisabled$;
 
   @Input() public clickable: boolean = false;
+  public readonly isClickable$: BehaviorSubject<boolean> = this.tagStateService.isClickable$;
 
   @Input() public tabIndex: number = 0;
+  public readonly tabIndex$: BehaviorSubject<number> = this.tagStateService.tabIndex$;
 
-  public get tabIndexAttribute(): null | number {
-    if (this.clickable) {
-      return this.tabIndex;
-    } else {
-      return null;
-    }
+  @Input() public color: TagColor = 'default';
+  private readonly color$: BehaviorSubject<TagColor> = this.tagStateService.color$;
+
+  @Input() public size: TagSize = 'medium';
+  private readonly size$: BehaviorSubject<TagSize> = this.tagStateService.size$;
+
+  public readonly colorClass$: Observable<string> = this.color$.pipe(map((color: TagColor) => `tag_${color}`));
+  public readonly sizeClass$: Observable<string> = this.size$.pipe(map((size: TagSize) => `tag_${size}`));
+
+  public get tabIndexAttribute(): Nullable<number> {
+    return this.clickable ? this.tabIndex : null;
   }
 
   public readonly withAvatar$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  constructor(private readonly tagStateService: TagStateService) {}
+
+  public ngOnChanges(changes: ComponentChanges<this>): void {
+    this.processDisabledChange(changes?.disabled);
+    this.processClickableChange(changes?.clickable);
+    this.processTabIndexChange(changes?.tabIndex);
+    this.processColorChange(changes?.color);
+    this.processSizeChange(changes?.size);
+  }
 
   public ngAfterContentInit(): void {
     this.processWithAvatarChange();
@@ -68,5 +69,55 @@ export class TagComponent implements AfterContentInit {
   private processWithAvatarChange(): void {
     const withAvatar: boolean = this.avatarComponent !== undefined;
     this.withAvatar$.next(withAvatar);
+  }
+
+  private processDisabledChange(change: ComponentChange<this, boolean>): void {
+    const updatedValue: boolean | undefined = change?.currentValue;
+
+    if (isNil(updatedValue)) {
+      return;
+    }
+
+    this.isDisabled$.next(updatedValue);
+  }
+
+  private processClickableChange(change: ComponentChange<this, boolean>): void {
+    const updatedValue: boolean | undefined = change?.currentValue;
+
+    if (isNil(updatedValue)) {
+      return;
+    }
+
+    this.isClickable$.next(updatedValue);
+  }
+
+  private processTabIndexChange(change: ComponentChange<this, number>): void {
+    const updatedValue: number | undefined = change?.currentValue;
+
+    if (isNil(updatedValue)) {
+      return;
+    }
+
+    this.tabIndex$.next(updatedValue);
+  }
+
+  private processColorChange(change: ComponentChange<this, TagColor>): void {
+    const updatedValue: TagColor | undefined = change?.currentValue;
+
+    if (isNil(updatedValue)) {
+      return;
+    }
+
+    this.color$.next(updatedValue);
+  }
+
+  private processSizeChange(change: ComponentChange<this, TagSize>): void {
+    const updatedValue: TagSize | undefined = change?.currentValue;
+
+    if (isNil(updatedValue)) {
+      return;
+    }
+
+    this.size$.next(updatedValue);
   }
 }
