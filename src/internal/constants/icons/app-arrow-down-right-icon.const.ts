9a3d0a76d24059749b6c3b8b7e89b15025c25972
gaevivan <6507085-gaevivan@users.noreply.gitlab.com>
2022-01-14 17:23:45 +0300

    feature: BIM-16279 new icon arrow down right

---
+++ src/internal/constants/icons/app-arrow-down-right-icon.const.ts
@@ -0,0 +1,10 @@
+import { IconDefinition } from '../../declarations/interfaces/icon-definition.interface';
+
+export const appArrowDownRightIcon: IconDefinition = {
+  name: 'app-arrow-down-right',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5 4L12 12" stroke="currentColor"/>
+  <path d="M12 4V12H4" stroke="currentColor"/>
+  </svg>
+`,
+};
