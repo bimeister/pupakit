9bd4e729a80c8339637ea211dcc2a23531df26cf
Vladilen Zamyshlyaev <vladilen.zamyshlyaev@bimeister.com>
2022-02-01 17:09:00 +0300

    feature: add full mode to counter

--- src/lib/components/counter/components/counter/counter.component.ts
+++ src/lib/components/counter/components/counter/counter.component.ts
@@ -1,14 +1,16 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 import { CountColor } from '../../../../../internal/declarations/types/count-color.type';
+import { CounterMode } from '../../../../../internal/declarations/types/counter-mode.type';
 
 @Component({
   selector: 'pupa-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class CounterComponent {
   @Input() public count: number;
-
   @Input() public color: CountColor = 'neutral';
+  @Input() public mode: CounterMode = 'default';
 }
