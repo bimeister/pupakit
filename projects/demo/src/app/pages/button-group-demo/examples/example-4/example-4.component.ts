39bae7e799ae42234708db7e5bd8fa8f7e178be2
smurygin <d.a.smurygin@gmail.com>
2022-03-23 07:25:48 +0000

    feature: BIM-17713 refactor button-group demo page

---
+++ projects/demo/src/app/pages/button-group-demo/examples/example-4/example-4.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'demo-button-group-example-4',
+  templateUrl: './example-4.component.html',
+  styleUrls: ['./example-4.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonGroupExample4Component {}
