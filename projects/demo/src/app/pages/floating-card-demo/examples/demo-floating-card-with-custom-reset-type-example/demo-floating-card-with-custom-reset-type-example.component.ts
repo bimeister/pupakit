442374aa4ba33278ec2d69d86f0cc94e96beff3b
devfakNode123 <devfak.node@gmail.com>
2022-04-01 10:59:36 +0300

    feature: BIM-17718 Add examples page for floating card

---
+++ projects/demo/src/app/pages/floating-card-demo/examples/demo-floating-card-with-custom-reset-type-example/demo-floating-card-with-custom-reset-type-example.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'pupa-demo-floating-card-with-custom-reset-type-example',
+  templateUrl: './demo-floating-card-with-custom-reset-type-example.component.html',
+  styleUrls: ['./demo-floating-card-with-custom-reset-type-example.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DemoFloatingCardWithCustomResetTypeExampleComponent {}
