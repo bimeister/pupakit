97f2764c79184dd7c8c8cab93ce84c8f02a820a8
denis.okishev <denis.okishev@bimeister.com>
2022-04-14 15:25:28 +0300

    feature: BIM-19310 Day-selector demo page

--- projects/demo/src/app/pages/selector-demo/selector-demo.module.ts
+++ projects/demo/src/app/pages/selector-demo/selector-demo.module.ts
@@ -3,9 +3,17 @@ import { DemoSharedModule } from '../../shared/shared.module';
 import { SelectorDemoRoutingModule } from './selector-demo-routing.module';
 import { SelectorDemoComponent } from './selector-demo.component';
 import { SharedModule } from '@kit/internal/shared/shared.module';
+import { DemoPlainDaySelectorExampleComponent } from './examples/demo-plain-day-selector-example/demo-plain-day-selector-example.component';
+import { DemoItemSizeDaySelectorExampleComponent } from './examples/demo-item-size-day-selector-example/demo-item-size-day-selector-example.component';
+import { DemoLocaleDaySelectorExampleComponent } from './examples/demo-locale-day-selector-example/demo-locale-day-selector-example.component';
 
 @NgModule({
-  declarations: [SelectorDemoComponent],
+  declarations: [
+    SelectorDemoComponent,
+    DemoPlainDaySelectorExampleComponent,
+    DemoItemSizeDaySelectorExampleComponent,
+    DemoLocaleDaySelectorExampleComponent,
+  ],
   imports: [DemoSharedModule, SelectorDemoRoutingModule, SharedModule],
 })
 export class SelectorDemoModule {}
