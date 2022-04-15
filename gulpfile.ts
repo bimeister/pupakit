7df1526662032fba7a12e178b0ca76de8d57c0ef
ScarletFlash <scarletflash.dev@gmail.com>
2022-04-15 19:50:10 +0300

    refactor: remove fixImportsTask gulp-task

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,10 +1,17 @@
-import { parallel, series, task } from 'gulp';
+import { parallel, series, task, TaskFunction } from 'gulp';
+import { executeCommandWithLogging } from './build/execute-command-with-logging';
 import { buildDemoStatic } from './build/gulp/build-demo-static.task';
 import { buildLibraryTask } from './build/gulp/build-library.task';
 import { bundleAssetsTask } from './build/gulp/bundle-assets.task';
 import { bundleStyleFilesTask } from './build/gulp/bundle-style-files.task';
 
-import { fixImportsTask } from './build/gulp/fix-imports.task';
+function fixImportsTask(): TaskFunction {
+  return (onDone: VoidFunction) =>
+    executeCommandWithLogging('npm run replace:all', {
+      onDone,
+      printDefaultOutput: false,
+    });
+}
 
 task('build', series(buildLibraryTask(), parallel(bundleAssetsTask(), bundleStyleFilesTask()), fixImportsTask()));
 
