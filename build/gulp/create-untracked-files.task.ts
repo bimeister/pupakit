92f17b34105960394189b250e75402745bcb7fbf
Fedor Usakov <fedor.usakov@bimeister.com>
2022-04-15 19:18:28 +0300

    refactor: remove createColorsMapTask & createUntrackedFilesTask gulp-tasks

--- build/gulp/create-untracked-files.task.ts
+++
@@ -1,6 +0,0 @@
-import { parallel, TaskFunction } from 'gulp';
-import { createColorsMapTask } from './create-colors-map.task';
-
-export function createUntrackedFilesTask(): TaskFunction {
-  return parallel(createColorsMapTask());
-}
