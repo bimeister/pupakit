2e560fcb31ac0c1d2b098cbbad22053604ca62f9
ScarletFlash <scarletflash.dev@gmail.com>
2022-04-15 18:37:17 +0300

    refactor: remove createBarrelFilesTask gulp-task

--- build/gulp/create-untracked-files.task.ts
+++ build/gulp/create-untracked-files.task.ts
@@ -1,8 +1,7 @@
 import { parallel, TaskFunction } from 'gulp';
 import { createAdaptiveVariablesTask } from './create-adaptive-variables.task';
-import { createBarrelFilesTask } from './create-barrel-files.task';
 import { createColorsMapTask } from './create-colors-map.task';
 
 export function createUntrackedFilesTask(): TaskFunction {
-  return parallel(createBarrelFilesTask(), createAdaptiveVariablesTask(), createColorsMapTask());
+  return parallel(createAdaptiveVariablesTask(), createColorsMapTask());
 }
--- gulpfile.ts
+++ gulpfile.ts
@@ -3,19 +3,11 @@ import { buildDemoStatic } from './build/gulp/build-demo-static.task';
 import { buildLibraryTask } from './build/gulp/build-library.task';
 import { bundleAssetsTask } from './build/gulp/bundle-assets.task';
 import { bundleStyleFilesTask } from './build/gulp/bundle-style-files.task';
-import { createBarrelFilesTask } from './build/gulp/create-barrel-files.task';
+
 import { createUntrackedFilesTask } from './build/gulp/create-untracked-files.task';
 import { fixImportsTask } from './build/gulp/fix-imports.task';
 
-task(
-  'build',
-  series(
-    createBarrelFilesTask(),
-    buildLibraryTask(),
-    parallel(bundleAssetsTask(), bundleStyleFilesTask()),
-    fixImportsTask()
-  )
-);
+task('build', series(buildLibraryTask(), parallel(bundleAssetsTask(), bundleStyleFilesTask()), fixImportsTask()));
 
 task('create:untracked-files', createUntrackedFilesTask());
 
