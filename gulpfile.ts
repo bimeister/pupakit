95e247772a07f9e2d235548fb9f48e23481d3548
ScarletFlash <scarletflash.dev@gmail.com>
2022-04-15 19:18:28 +0300

    refactor: remove createColorsMapTask & createUntrackedFilesTask gulp-tasks

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,11 +4,8 @@ import { buildLibraryTask } from './build/gulp/build-library.task';
 import { bundleAssetsTask } from './build/gulp/bundle-assets.task';
 import { bundleStyleFilesTask } from './build/gulp/bundle-style-files.task';
 
-import { createUntrackedFilesTask } from './build/gulp/create-untracked-files.task';
 import { fixImportsTask } from './build/gulp/fix-imports.task';
 
 task('build', series(buildLibraryTask(), parallel(bundleAssetsTask(), bundleStyleFilesTask()), fixImportsTask()));
-
-task('create:untracked-files', createUntrackedFilesTask());
 
 task('build-demo-static', buildDemoStatic());
