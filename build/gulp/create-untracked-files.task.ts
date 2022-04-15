8f6b7cef57ffb7349cb9bcc2125943efa582ea26
Fedor Usakov <fedor.usakov@bimeister.com>
2022-04-15 19:07:11 +0300

    refactor: remove createAdaptiveVariablesTask gulp-task

--- build/gulp/create-untracked-files.task.ts
+++ build/gulp/create-untracked-files.task.ts
@@ -1,7 +1,6 @@
 import { parallel, TaskFunction } from 'gulp';
-import { createAdaptiveVariablesTask } from './create-adaptive-variables.task';
 import { createColorsMapTask } from './create-colors-map.task';
 
 export function createUntrackedFilesTask(): TaskFunction {
-  return parallel(createAdaptiveVariablesTask(), createColorsMapTask());
+  return parallel(createColorsMapTask());
 }
