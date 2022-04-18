e3a468b3d5286df60b2d6930d83ac63f57adff47
Fedor Usakov <fedor.usakov@bimeister.com>
2022-04-18 12:30:15 +0300

    refactor: remove buildLibraryTask gulp-task

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,24 +1,4 @@
-import { parallel, series, task, TaskFunction } from 'gulp';
-import { executeCommandWithLogging } from './build/execute-command-with-logging';
+import { task } from 'gulp';
 import { buildDemoStatic } from './build/gulp/build-demo-static.task';
-import { buildLibraryTask } from './build/gulp/build-library.task';
-
-function replaceSubstrings(): TaskFunction {
-  return (onDone: VoidFunction) =>
-    executeCommandWithLogging('npm run replace:all', {
-      onDone,
-      printDefaultOutput: false,
-    });
-}
-
-function bundleStaticFilesTask(): TaskFunction {
-  return (onDone: VoidFunction) =>
-    executeCommandWithLogging('npm run bundle:all', {
-      onDone,
-      printDefaultOutput: false,
-    });
-}
-
-task('build', series(buildLibraryTask(), parallel(bundleStaticFilesTask()), replaceSubstrings()));
 
 task('build-demo-static', buildDemoStatic());
