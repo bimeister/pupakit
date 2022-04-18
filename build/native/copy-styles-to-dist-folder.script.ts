90b6fc15acb36c6b236d848381db44a610f07ba4
ScarletFlash <scarletflash.dev@gmail.com>
2022-04-18 12:15:37 +0300

    refactor: remove bundleStyleFilesTask gulp-task

---
+++ build/native/copy-styles-to-dist-folder.script.ts
@@ -0,0 +1,66 @@
+import { existsSync, Stats } from 'fs';
+import { copyFile, mkdir, readdir, stat } from 'fs/promises';
+import { basename, join } from 'path';
+
+interface FileOrFolder {
+  stats: Stats;
+  path: string;
+  name: string;
+}
+async function copyRecursive(sourcePath: string, targetPath: string): Promise<void> {
+  if (!existsSync(targetPath)) {
+    await mkdir(targetPath, {
+      recursive: true,
+    });
+  }
+
+  const fileOrFolderPaths: string[] = await readdir(sourcePath).then((fileOrFolderNames: string[]) =>
+    fileOrFolderNames.map((name: string) => join(sourcePath, name))
+  );
+  const filesOrFolders: FileOrFolder[] = await Promise.all(
+    fileOrFolderPaths.map(async (path: string) => {
+      const stats: Stats = await stat(path);
+      const name: string = basename(path);
+      return { stats, path, name };
+    })
+  );
+
+  filesOrFolders.forEach(async ({ stats, path, name }: FileOrFolder) => {
+    const resultTargetPath: string = join(targetPath, name);
+
+    if (stats.isFile()) {
+      await copyFile(path, resultTargetPath);
+      return;
+    }
+
+    if (!stats.isDirectory()) {
+      throw new Error(`There is something strange on path: ${path} — not a file and not a directory.`);
+    }
+
+    if (!existsSync(resultTargetPath)) {
+      await mkdir(resultTargetPath, {
+        recursive: true,
+      });
+    }
+
+    await copyRecursive(path, resultTargetPath);
+  });
+}
+
+async function copyStylesToDistFolder(): Promise<void> {
+  const sourcePath: string = './projects/kit/src/styles/';
+  const targetPath: string = './dist/lib/styles/';
+
+  if (!existsSync(sourcePath)) {
+    throw new Error('Source path does not exit.');
+  }
+
+  const sourceStats: Stats = await stat(sourcePath);
+  if (!sourceStats.isDirectory()) {
+    throw new Error('There is no directory on the source path.');
+  }
+
+  await copyRecursive(sourcePath, targetPath);
+}
+
+Promise.resolve().then(() => copyStylesToDistFolder());
--- gulpfile.ts
+++ gulpfile.ts
@@ -3,11 +3,18 @@ import { executeCommandWithLogging } from './build/execute-command-with-logging'
 import { buildDemoStatic } from './build/gulp/build-demo-static.task';
 import { buildLibraryTask } from './build/gulp/build-library.task';
 import { bundleAssetsTask } from './build/gulp/bundle-assets.task';
-import { bundleStyleFilesTask } from './build/gulp/bundle-style-files.task';
 
 function fixImportsTask(): TaskFunction {
   return (onDone: VoidFunction) =>
     executeCommandWithLogging('npm run replace:all', {
+      onDone,
+      printDefaultOutput: false,
+    });
+}
+
+function bundleStyleFilesTask(): TaskFunction {
+  return (onDone: VoidFunction) =>
+    executeCommandWithLogging('npm run bundle:styles', {
       onDone,
       printDefaultOutput: false,
     });
