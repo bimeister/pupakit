30bb7d9c83cf4d2add00084a94cec2d3faea572d
ScarletFlash <scarletflash.dev@gmail.com>
2022-04-19 12:24:40 +0300

    fix: invalid package.json reference to ts-node

--- build/node/copy-recursive.ts
+++ build/node/copy-recursive.ts
@@ -1,3 +1,5 @@
+import { VOID } from '@bimeister/utilities/constants';
+import { log } from 'console';
 import { existsSync, Stats } from 'fs';
 import { copyFile, mkdir, readdir, stat } from 'fs/promises';
 import { basename, join } from 'path';
@@ -9,9 +11,9 @@ interface FileOrFolder {
 }
 export async function copyRecursive(sourcePath: string, targetPath: string): Promise<void> {
   if (!existsSync(targetPath)) {
-    await mkdir(targetPath, {
+    return await mkdir(targetPath, {
       recursive: true,
-    });
+    }).then(() => VOID);
   }
 
   const fileOrFolderPaths: string[] = await readdir(sourcePath).then((fileOrFolderNames: string[]) =>
@@ -25,24 +27,25 @@ export async function copyRecursive(sourcePath: string, targetPath: string): Pro
     })
   );
 
-  filesOrFolders.forEach(async ({ stats, path, name }: FileOrFolder) => {
-    const resultTargetPath: string = join(targetPath, name);
+  return Promise.all(
+    filesOrFolders.map(async ({ stats, path, name }: FileOrFolder) => {
+      const resultTargetPath: string = join(targetPath, name);
 
-    if (stats.isFile()) {
-      await copyFile(path, resultTargetPath);
-      return;
-    }
+      if (stats.isFile()) {
+        return await copyFile(path, resultTargetPath).then(() => log(`[COPIED] ${path} ≫ ${resultTargetPath}`));
+      }
 
-    if (!stats.isDirectory()) {
-      throw new Error(`There is something strange on path: ${path} — not a file and not a directory.`);
-    }
+      if (!stats.isDirectory()) {
+        throw new Error(`There is something strange on path: ${path} — not a file and not a directory.`);
+      }
 
-    if (!existsSync(resultTargetPath)) {
-      await mkdir(resultTargetPath, {
-        recursive: true,
-      });
-    }
+      if (!existsSync(resultTargetPath)) {
+        return await mkdir(resultTargetPath, {
+          recursive: true,
+        });
+      }
 
-    await copyRecursive(path, resultTargetPath);
-  });
+      await copyRecursive(path, resultTargetPath);
+    })
+  ).then(() => VOID);
 }
