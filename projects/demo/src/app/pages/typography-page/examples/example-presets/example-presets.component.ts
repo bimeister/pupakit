8311ec2154ca07332ed21938817053a443ccf295
Mikhail Kirichenko <mikhail.kirichenko@bimeister.com>
2022-05-02 19:44:49 +0300

    feature: add new font

---
+++ projects/demo/src/app/pages/typography-page/examples/example-presets/example-presets.component.ts
@@ -0,0 +1,50 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ToastsService } from '@kit/internal/shared/services/toasts.service';
+import { Clipboard } from '@angular/cdk/clipboard';
+
+interface Preset {
+  mixin: string;
+  class: string;
+  title: string;
+}
+
+@Component({
+  selector: 'demo-typography-example-presets',
+  templateUrl: './example-presets.component.html',
+  styleUrls: ['./example-presets.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TypographyPresetsComponent {
+  public readonly headingPresets: Preset[] = Array(6)
+    .fill(undefined)
+    .map((_: undefined, index: number) => ({
+      class: `font-preset-h${index + 1}`,
+      mixin: `@include font-preset-h${index + 1};`,
+      title: `h${index + 1}: PupaKit is an open source collection of Angular components`,
+    }));
+
+  public readonly captionPresets: Preset[] = ['m', 's', 'xs', 'xxs'].map((item: string) => ({
+    class: `font-preset-caption-${item}`,
+    mixin: `@include font-preset-caption-${item}};`,
+    title: `caption-${item}: PupaKit is an open source collection of Angular components`,
+  }));
+
+  public readonly bodyPresets: Preset[] = ['m', 's'].map((item: string) => ({
+    class: `font-preset-body-${item}`,
+    mixin: `@include font-preset-body-${item}};`,
+    title: `body-${item}: PupaKit is an open source collection of Angular components`,
+  }));
+
+  constructor(private readonly cdkClipboard: Clipboard, private readonly toastsService: ToastsService) {}
+
+  public copyToClipBoard(mixin: string): void {
+    this.cdkClipboard.copy(mixin);
+    this.toastsService.open({
+      data: {
+        bodyText: 'Copied to clipboard',
+        type: 'info',
+      },
+    });
+  }
+}
