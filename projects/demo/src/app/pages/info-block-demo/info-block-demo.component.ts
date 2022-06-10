80f4ff63acbc544e26f1d6d8525d9a005fa41b90
JuliChekulaeva <www.julia.ru25@gmail.com>
2022-06-10 09:49:03 +0000

    feature: BIM-17720 update demo page of info block

--- projects/demo/src/app/pages/info-block-demo/info-block-demo.component.ts
+++ projects/demo/src/app/pages/info-block-demo/info-block-demo.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { PropsOption } from '../../shared/components/example-viewer/declarations/interfaces/props.option';
 
 const BASE_REQUEST_PATH: string = 'info-block-demo/examples';
 
@@ -13,4 +14,28 @@ export class InfoBlockDemoComponent {
   public readonly example1Content: Record<string, string> = {
     HTML: `${BASE_REQUEST_PATH}/example-1/example-1.component.html`,
   };
+
+  public readonly variantOptions: PropsOption[] = [
+    {
+      caption: 'Info',
+      value: 'info',
+      isDefault: true,
+    },
+    {
+      caption: 'Note',
+      value: 'note',
+    },
+    {
+      caption: 'Success',
+      value: 'success',
+    },
+    {
+      caption: 'Warning',
+      value: 'warning',
+    },
+    {
+      caption: 'Error',
+      value: 'error',
+    },
+  ];
 }
