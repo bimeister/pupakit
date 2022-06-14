3cd12e28c69c0d96ec6371b3650cc50677c4c41d
JuliChekulaeva <www.julia.ru25@gmail.com>
2022-06-14 08:41:31 +0000

    feature: BIM-17722 update demo page of label

--- projects/demo/src/app/pages/label-demo/label-demo.component.ts
+++ projects/demo/src/app/pages/label-demo/label-demo.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { PropsOption } from '../../shared/components/example-viewer/declarations/interfaces/props.option';
+
+const BASE_REQUEST_PATH: string = 'label-demo/examples';
 
 @Component({
   selector: 'demo-label',
@@ -11,4 +14,28 @@ export class LabelDemoComponent {
   public readonly buttonIconFormControl: FormControl = new FormControl('app-info');
 
   public readonly textFormControl: FormControl = new FormControl('Label Text');
+
+  public readonly sizeOptions: PropsOption[] = [
+    {
+      caption: 'Large',
+      value: 'large',
+    },
+    {
+      caption: 'Medium',
+      value: 'medium',
+      isDefault: true,
+    },
+    {
+      caption: 'Small',
+      value: 'small',
+    },
+  ];
+
+  public readonly basicExampleContent: Record<string, string> = {
+    HTML: `${BASE_REQUEST_PATH}/label-demo-basic-example/label-demo-basic-example.component.html`,
+  };
+  public readonly sizeExampleContent: Record<string, string> = {
+    HTML: `${BASE_REQUEST_PATH}/label-demo-example-size/label-demo-example-size.component.html`,
+    SCSS: `${BASE_REQUEST_PATH}/label-demo-example-size/label-demo-example-size.component.scss`,
+  };
 }
