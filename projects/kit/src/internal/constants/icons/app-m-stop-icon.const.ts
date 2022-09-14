88c6aa3e8c9cea420d6d623ae4f793298ed99b81
webgangster0 <aleksey.akulov@outlook.com>
2022-09-15 01:32:23 +0300

    feature: add media icons

---
+++ projects/kit/src/internal/constants/icons/app-m-stop-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../declarations/interfaces/icon-definition.interface';
+
+export const appMStopIcon: IconDefinition = {
+  name: 'app-m-stop',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M3.83333 2C2.82674 2 2 2.82674 2 3.83333V12.1667C2 13.1733 2.82674 14 3.83333 14H12.1667C13.1733 14 14 13.1733 14 12.1667V3.83333C14 2.82674 13.1733 2 12.1667 2H3.83333Z" fill="currentColor"/>
+  </svg>
+  `,
+};
