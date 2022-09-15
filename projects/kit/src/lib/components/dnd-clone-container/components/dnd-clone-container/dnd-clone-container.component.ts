4e11d57a4a0bc36f3e47933a868d38a83d710d50
Fedor Pushkov <fedor.pushkov@bimeister.com>
2022-09-16 00:31:37 +0700

    feature: BIM-23817 change pupa-table colors to semantic-colors and wrap dnd-clone-container component in theme-wrapper

--- projects/kit/src/lib/components/dnd-clone-container/components/dnd-clone-container/dnd-clone-container.component.ts
+++ projects/kit/src/lib/components/dnd-clone-container/components/dnd-clone-container/dnd-clone-container.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, Inject, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Optional, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { ThemeWrapperService } from '../../../theme-wrapper/services/theme-wrapper.service';
+import { Observable, of } from 'rxjs';
 import { DND_CLONE_CONTAINER_DATA_TOKEN } from '../../../../../internal/constants/tokens/dnd-clone-container-data.token';
 import { DndCloneContainerData } from '../../../../../internal/declarations/interfaces/dnd-clone-container-data.interface';
 
@@ -13,5 +15,11 @@ export class DndCloneContainerComponent<C> {
   public readonly templateRef: TemplateRef<C> = this.data.templateRef;
   public readonly templateContext: C = this.data.templateContext;
 
-  constructor(@Inject(DND_CLONE_CONTAINER_DATA_TOKEN) private readonly data: DndCloneContainerData<C>) {}
+  public readonly themeClass$: Observable<string> = this.themeWrapperService?.themeClass$ ?? of('');
+  public readonly theme$: Observable<string> = this.themeWrapperService?.theme$;
+
+  constructor(
+    @Inject(DND_CLONE_CONTAINER_DATA_TOKEN) private readonly data: DndCloneContainerData<C>,
+    @Optional() private readonly themeWrapperService: ThemeWrapperService
+  ) {}
 }
--- projects/kit/src/lib/components/dnd-clone-container/dnd-clone-container.module.ts
+++ projects/kit/src/lib/components/dnd-clone-container/dnd-clone-container.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ThemeWrapperModule } from '../theme-wrapper/theme-wrapper.module';
 import { DndCloneContainerComponent } from './components/dnd-clone-container/dnd-clone-container.component';
 
 @NgModule({
   declarations: [DndCloneContainerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ThemeWrapperModule],
   exports: [DndCloneContainerComponent],
 })
 export class DndCloneContainerModule {}
