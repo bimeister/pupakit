6a99780ec0e999584c28a19909f3b4f7cfbc2643
Aleksey Akulov <aleksey.akulov@bimeister.com>
2022-09-22 20:35:18 +0300

    feature: add step back icon

---
+++ projects/kit/src/internal/constants/icons/app-m-step-back-icon.const.ts
@@ -0,0 +1,8 @@
+import { IconDefinition } from '../../declarations/interfaces/icon-definition.interface';
+
+export const appMStepBackIcon: IconDefinition = {
+  name: 'app-m-step-back',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4.33002 8.00429C4.33002 8.16054 4.40424 8.30507 4.52924 8.40273L10.8613 13.2348C10.9511 13.3012 11.0566 13.3363 11.166 13.3363C11.2402 13.3363 11.3144 13.3207 11.3847 13.2855C11.5566 13.2035 11.666 13.0277 11.666 12.8363V3.17226C11.666 2.98085 11.5566 2.80507 11.3847 2.72304C11.2167 2.6371 11.0136 2.65664 10.8613 2.77382L4.52924 7.60585C4.40424 7.70351 4.33002 7.84804 4.33002 8.00429Z" fill="currentColor"/>
+    </svg>`,
+};
