6075f943871849f4a39ca45da8d96ec6fb12844a
vbabutskiy <vbabutskiy@gmail.com>
2022-11-28 11:57:46 +0300

    feature: BIM-25542 add icons and dynamic size support to avatar

---
+++ projects/demo/src/app/pages/avatar-demo/examples/example-7/example-7.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'demo-avatar-example-7',
+  templateUrl: './example-7.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AvatarExample7Component {}
