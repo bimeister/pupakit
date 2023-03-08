d7ba86795df1cffd3b4dcf4d00cdab80cf1c4a01
vsezol <vsezold@gmail.com>
2023-03-08 10:53:32 +0300

    feature: add calendar project

---
+++ projects/calendar/src/calendar.module.ts
@@ -0,0 +1,10 @@
+import { NgModule, Type } from '@angular/core';
+
+const MODULES: Type<unknown>[] = [];
+
+@NgModule({
+  declarations: [],
+  imports: MODULES,
+  exports: MODULES,
+})
+export class PupaCalendarModule {}
