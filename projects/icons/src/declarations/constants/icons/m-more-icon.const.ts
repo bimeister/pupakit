1ca979c3e18353279a7ad6231700f9e2046a597a
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-03-13 11:45:27 +0300

    feature: add new icons

---
+++ projects/icons/src/declarations/constants/icons/m-more-icon.const.ts
@@ -0,0 +1,12 @@
+import { IconDefinition } from '../../../declarations/interfaces/icon-definition.interface';
+
+export const mMoreIcon: IconDefinition = {
+  name: 'm-more',
+  data: `
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8 4.5C7.44772 4.5 7 4.05228 7 3.5C7 2.94772 7.44772 2.5 8 2.5C8.55228 2.5 9 2.94772 9 3.5C9 4.05228 8.55228 4.5 8 4.5Z" fill="currentColor"/>
+<path d="M7 12.5C7 13.0523 7.44772 13.5 8 13.5C8.55228 13.5 9 13.0523 9 12.5C9 11.9477 8.55228 11.5 8 11.5C7.44772 11.5 7 11.9477 7 12.5Z" fill="currentColor"/>
+<path d="M7 8C7 8.55228 7.44772 9 8 9C8.55228 9 9 8.55228 9 8C9 7.44772 8.55228 7 8 7C7.44772 7 7 7.44772 7 8Z" fill="currentColor"/>
+</svg>
+`,
+};
