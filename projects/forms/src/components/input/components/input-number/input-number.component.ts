af1ce8d1715555cc1d82eb546ce9e38a0f096516
Denis Svirko <dzianis.svirko@gmail.com>
2023-03-13 15:28:15 +0300

    fix: BIM-25725 set delegatesFocus for InputNumberComponent

--- projects/forms/src/components/input/components/input-number/input-number.component.ts
+++ projects/forms/src/components/input/components/input-number/input-number.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { isNil } from '@bimeister/utilities';
 import { InputBase } from '../../../../declarations/classes/abstract/input-base.abstract';
 import { ValueType } from '../../../../declarations/types/input-value.type';
@@ -7,10 +7,17 @@ import { ValueType } from '../../../../declarations/types/input-value.type';
   selector: 'pupa-input-number',
   templateUrl: './input-number.component.html',
   styleUrls: ['./input-number.component.scss'],
-  encapsulation: ViewEncapsulation.Emulated,
+  encapsulation: ViewEncapsulation.ShadowDom,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputNumberComponent extends InputBase<ValueType> {
+export class InputNumberComponent extends InputBase<ValueType> implements AfterViewInit {
+  public ngAfterViewInit(): void {
+    if (this.inputElementRef.nativeElement.shadowRoot) {
+      const input = this.inputElementRef.nativeElement.shadowRoot?.querySelector('input');
+      input.setAttribute('delegatesFocus', '');
+    }
+  }
+
   public setValue(value: ValueType): void {
     const serializedValue: string = isNil(value) ? '' : String(value);
     this.value$.next(serializedValue);
