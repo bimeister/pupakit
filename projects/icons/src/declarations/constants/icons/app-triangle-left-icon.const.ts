ce88e27dbce3111dd2e837c427df7e8c9ecb749f
disamiron <denis.mironov@ddgcorp.ru>
2023-04-10 14:20:04 +0300

    feature: add left and right triangle icon

---
+++ projects/icons/src/declarations/constants/icons/app-triangle-left-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../interfaces/icon-definition.interface';
+
+export const appTriangleLeftIcon: IconDefinition = {
+  name: 'app-triangle-left',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6.85354 8.35355C6.65828 8.15829 6.65828 7.84171 6.85354 7.64644L9.14643 5.35355C9.46142 5.03857 9.99999 5.26165 9.99999 5.7071L9.99999 10.2929C9.99999 10.7383 9.46142 10.9614 9.14643 10.6464L6.85354 8.35355Z" fill="currentColor"/>
+  </svg>
+`,
+};
