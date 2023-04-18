f09b301d3d365a8cf6aaeda8475c525beb6706f8
Фёдор Девиченский <fedor.devichensky@bimeister.com>
2023-04-18 17:02:43 +0700

    feature: add new icons for pupa tree

---
+++ projects/icons/src/declarations/constants/icons/app-lock-filled-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../../declarations/interfaces/icon-definition.interface';
+
+export const appLockFilledIcon: IconDefinition = {
+  name: 'app-lock-filled',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M7.98935 0.977417C6.17805 0.977417 4.7097 2.44577 4.7097 4.25707V5.6634C3.6503 5.89264 2.8566 6.83527 2.8566 7.96328V11.6695C2.8566 12.9691 3.91012 14.0226 5.2097 14.0226H10.769C12.0686 14.0226 13.1221 12.9691 13.1221 11.6695V7.96328C13.1221 6.83527 12.3284 5.89264 11.269 5.6634V4.25707C11.269 2.44577 9.80066 0.977417 7.98935 0.977417ZM6.63625 4.25707C6.63625 3.50977 7.24206 2.90397 7.98935 2.90397C8.73665 2.90397 9.34246 3.50977 9.34246 4.25707V5.61017H6.63625V4.25707Z" fill="currentColor"/>
+</svg>
+`,
+};
---
+++ projects/icons/src/declarations/constants/icons/app-user-filled-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../../declarations/interfaces/icon-definition.interface';
+
+export const appUserFilledIcon: IconDefinition = {
+  name: 'app-user-filled',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M7.95 1.59863C9.75 1.59863 11.15 2.99863 11.15 4.79862C11.15 6.59862 9.75 7.99862 7.95 7.99862C6.15 7.99862 4.75 6.49862 4.75 4.79862C4.75 3.09863 6.15 1.59863 7.95 1.59863ZM13.75 10.5987C13.75 12.3987 11.35 15 7.95 15C4.55 15 2.25 12.3987 2.25 10.5987V10.1987C2.25 9.49866 2.75 8.99866 3.45 8.99866H12.55C13.25 8.99866 13.75 9.49866 13.75 10.1987V10.5987Z" fill="currentColor"/>
+  </svg>
+`,
+};
