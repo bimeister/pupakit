c43ffb8bb6e56f11966230295754c43dd6dfcac4
Georgyi <georgy.melomordanov@bimeister.com>
2023-05-15 16:55:33 +0300

    feature: add sidebars icons

---
+++ projects/icons/src/declarations/constants/icons/app-hide-sidebar-left.const.ts
@@ -0,0 +1,10 @@
+import { IconDefinition } from '../../interfaces/icon-definition.interface';
+
+export const appHideSidebarLeftIcon: IconDefinition = {
+  name: 'app-hide-sidebar-left',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9.522 6.172a.5.5 0 1 1 .707.707l-.62.62h2.157a.5.5 0 1 1 0 1H9.608l.621.622a.5.5 0 1 1-.707.707L8.047 8.353a.5.5 0 0 1 0-.707l1.475-1.474Z" fill="currentColor"/>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M14.418 4.002a2 2 0 0 0-2-2H3.62a2 2 0 0 0-2 2v7.995a2 2 0 0 0 2 2h8.797a2 2 0 0 0 2-2V4.002Zm-10.797-1h2.035v9.995H3.621a1 1 0 0 1-1-1V4.002a1 1 0 0 1 1-1Zm3.035 9.995h5.762a1 1 0 0 0 1-1V4.002a1 1 0 0 0-1-1H6.656v9.995Z" fill="currentColor"/>
+</svg>
+`,
+};
---
+++ projects/icons/src/declarations/constants/icons/app-open-sidebar-right.const.ts
@@ -0,0 +1,10 @@
+import { IconDefinition } from '../../interfaces/icon-definition.interface';
+
+export const appOpenSidebarRightIcon: IconDefinition = {
+  name: 'app-open-sidebar-right',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9.716 6.172a.5.5 0 0 0-.707.707l.62.62H7.472a.5.5 0 1 0 0 1H9.63l-.621.622a.5.5 0 0 0 .707.707l1.474-1.475a.5.5 0 0 0 0-.707L9.716 6.172Z" fill="currentColor"/>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M14.399 4.002a2 2 0 0 0-2-2H3.6a2 2 0 0 0-2 2v7.995a2 2 0 0 0 2 2h8.8a2 2 0 0 0 2-2V4.002Zm-9.943-1h-.855a1 1 0 0 0-1 1v7.995a1 1 0 0 0 1 1h.855V3.002Zm1 9.995H12.4a1 1 0 0 0 1-1V4.002a1 1 0 0 0-1-1H5.456v9.995Z" fill="currentColor"/>
+</svg>
+`,
+};
