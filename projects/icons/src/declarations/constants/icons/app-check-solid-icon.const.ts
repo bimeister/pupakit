85b62af7a5058c02c645234ca88e13d1a0dc2286
Фёдор Девиченский <fedor.devichensky@bimeister.com>
2023-05-31 16:31:39 +0700

    feature: Add new cross-solid and check-solid icons for expert-tree

---
+++ projects/icons/src/declarations/constants/icons/app-check-solid-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../../declarations/interfaces/icon-definition.interface';
+
+export const appCheckSolidIcon: IconDefinition = {
+  name: 'app-check-solid',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.2556 3.51428C13.8923 3.151 13.3033 3.151 12.94 3.51428L5.85437 10.5999L3.0383 7.78389C2.67502 7.42062 2.08604 7.42062 1.72277 7.7839C1.3595 8.14717 1.3595 8.73616 1.72278 9.09943L5.19662 12.5732C5.55989 12.9365 6.14887 12.9365 6.51214 12.5732L14.2556 4.82981C14.6188 4.46653 14.6188 3.87755 14.2556 3.51428Z" fill="currentColor"/>
+</svg>
+`,
+};
