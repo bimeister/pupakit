13ef5fe397ef5eb424b55cd378ca2164c8657ec5
Vyacheslav Chernov <vyacheslav.chernov@bimeister.com>
2023-06-19 13:49:16 +0300

    feature: BIM-33068 Prevent theme inverse for alerts

--- projects/overlays/src/declarations/interfaces/alert-module-config.interface.ts
+++ projects/overlays/src/declarations/interfaces/alert-module-config.interface.ts
@@ -1,5 +1,7 @@
 import { ComponentType } from '@angular/cdk/portal';
+import { ThemeBehavior } from '../enums/theme-behavior.enum';
 
 export interface AlertModuleConfig {
   readonly toolbarComponent: ComponentType<unknown> | null;
+  readonly themeBehaviour?: ThemeBehavior;
 }
