130ff2bbc6249e50d9eb4d372849b0b27a7cadf4
roman.veremyov <roman.veremyov@bimeister.com>
2023-06-28 18:43:43 +0300

    BIM-30184 - added arrow icons

--- projects/icons/src/declarations/constants/icons/app-arrow-down-icon.const.ts
+++ projects/icons/src/declarations/constants/icons/app-arrow-down-icon.const.ts
@@ -1,6 +1,6 @@
 import { IconDefinition } from '../../interfaces/icon-definition.interface';
 
-export const appArrowDown: IconDefinition = {
+export const appArrowDownIcon: IconDefinition = {
   name: 'app-arrow-down',
   data: `
   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
--- projects/icons/src/declarations/constants/icons/app-arrow-up-icon.const.ts
+++ projects/icons/src/declarations/constants/icons/app-arrow-up-icon.const.ts
@@ -1,6 +1,6 @@
 import { IconDefinition } from '../../interfaces/icon-definition.interface';
 
-export const appArrowUp: IconDefinition = {
+export const appArrowUpIcon: IconDefinition = {
   name: 'app-arrow-up',
   data: `
   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
