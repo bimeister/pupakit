6da0b3ba12c7705ba793f3e5947a297a297cd523
SanchouZ <sasha.harchikov@gmail.com>
2023-07-13 17:35:57 +0300

    fix: BIM-33099 Remove unneccessary data and rollback to async scheduler

--- projects/demo/src/app/pages/modal-demo/examples/modal-demo-example-7/modal-content/modal-demo-example-7.component.ts
+++ projects/demo/src/app/pages/modal-demo/examples/modal-demo-example-7/modal-content/modal-demo-example-7.component.ts
@@ -1,7 +1,4 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit, Optional, ViewEncapsulation } from '@angular/core';
-import { MODAL_DATA_TOKEN } from '../../../../../../declarations/tokens/modal-data.token';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { isNil, Nullable } from '@bimeister/utilities';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'pupa-modal-demo-example-7',
@@ -10,16 +7,4 @@ import { isNil, Nullable } from '@bimeister/utilities';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class ModalDemoExample7Component implements OnInit {
-  private readonly modalDataState$: BehaviorSubject<Nullable<any>> = new BehaviorSubject<Nullable<any>>(null);
-
-  public readonly modalData$: Observable<Nullable<any>> = this.modalDataState$.asObservable();
-
-  constructor(@Inject(MODAL_DATA_TOKEN) @Optional() private readonly modalData: any) {}
-
-  public ngOnInit(): void {
-    if (!isNil(this.modalData)) {
-      this.modalDataState$.next(this.modalData);
-    }
-  }
-}
+export class ModalDemoExample7Component {}
