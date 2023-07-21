02936f2bbec723e490f669ffa7c87b7a803c1177
Max Yarosh <jmksm96@gmail.com>
2023-07-21 17:52:29 +0300

    fix: BIM-32144 Removed popover reopening
    fix: BIM-32144 fixed memory leak
    
    fix: BIM-33878 updated after review
    
    fix: BIM-33878 refactoring
    
    fix: BIM-32144 Removed popover reopening
    
    fix: BIM-32144 fixed memory leak

--- projects/overlays/src/services/popovers.service.ts
+++ projects/overlays/src/services/popovers.service.ts
@@ -1,7 +1,7 @@
 import { Overlay } from '@angular/cdk/overlay';
 import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable, Injector, RendererFactory2 } from '@angular/core';
-import { ClientUiStateHandlerService } from '@bimeister/pupakit.common';
+import { ElementRef, Inject, Injectable, Injector, RendererFactory2 } from '@angular/core';
+import { ClientUiStateHandlerService, Position } from '@bimeister/pupakit.common';
 import { isNil } from '@bimeister/utilities';
 import { PopoverComponentBase } from '../declarations/classes/abstract/popover-component-base.abstract';
 import { OpenedPopover } from '../declarations/classes/opened-popover.class';
@@ -14,6 +14,7 @@ import { PortalLayersService } from './portal-layers.service';
 
 @Injectable({ providedIn: 'root' })
 export class PopoversService {
+  private readonly popoverRefByAnchor: Map<ElementRef<HTMLElement> | Position, Popover<any>> = new Map();
   constructor(
     @Inject(DOCUMENT) private readonly document: Document,
     private readonly clientUiStateHandlerService: ClientUiStateHandlerService,
@@ -26,22 +27,32 @@ export class PopoversService {
   public open<TComponent extends PopoverComponentBase<unknown, unknown>>(
     config: PopoverConfig<TComponent, PopoverDataType<TComponent>>
   ): OpenedPopover<PopoverReturnType<TComponent>> {
-    const popover: Popover<TComponent> = new Popover(
-      config,
-      this.overlay,
-      this.injector,
-      this.rendererFactory,
-      this.clientUiStateHandlerService,
-      this.document
-    );
+    const anchor: ElementRef<HTMLElement> | Position = config.anchor;
+    const isExisting: boolean = this.popoverRefByAnchor.has(anchor);
 
-    const popoverRef: PopoverRef<PopoverDataType<TComponent>, PopoverReturnType<TComponent>> = popover.open();
+    const popover: Popover<TComponent> = isExisting
+      ? this.popoverRefByAnchor.get(anchor)
+      : new Popover(
+          config,
+          this.overlay,
+          this.injector,
+          this.rendererFactory,
+          this.clientUiStateHandlerService,
+          this.document
+        );
 
-    this.portalLayersService.register(popover);
+    const popoverRef: PopoverRef<PopoverDataType<TComponent>, PopoverReturnType<TComponent>> = popover.open(isExisting);
+
+    if (!isExisting) {
+      this.popoverRefByAnchor.set(anchor, popover);
+      this.portalLayersService.register(popover);
+    }
+
     this.portalLayersService.moveToTopById(popover.id);
 
     popoverRef.closed$.subscribe(() => {
       this.portalLayersService.removeById(popover.id);
+      this.popoverRefByAnchor.delete(anchor);
     });
 
     return new OpenedPopover(popover.id, popoverRef);
