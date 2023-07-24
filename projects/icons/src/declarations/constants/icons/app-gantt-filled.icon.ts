6355ea313381beb933d1baa4bd23c10041ec9088
Andrey Gromozdov <andrey.gromozdov@bimeister.com>
2023-07-24 22:31:34 +0300

    fearure: Icons for navigation panel.

---
+++ projects/icons/src/declarations/constants/icons/app-gantt-filled.icon.ts
@@ -0,0 +1,12 @@
+import { IconDefinition } from '../../../declarations/interfaces/icon-definition.interface';
+
+export const appGanttFilledIcon: IconDefinition = {
+  name: 'app-gantt-filled',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M2.75 3C2.75 2.72386 2.52614 2.5 2.25 2.5C1.97386 2.5 1.75 2.72386 1.75 3V13C1.75 13.2761 1.97386 13.5 2.25 13.5H5.36381C5.63995 13.5 5.86381 13.2761 5.86381 13C5.86381 12.7239 5.63995 12.5 5.36381 12.5H2.75V3Z" fill="currentColor"/>
+  <path d="M5.25 2C4.69772 2 4.25 2.44772 4.25 3V4C4.25 4.55228 4.69772 5 5.25 5H10.25C10.8023 5 11.25 4.55228 11.25 4V3C11.25 2.44772 10.8023 2 10.25 2H5.25Z" fill="currentColor"/>
+  <path d="M5.25 7.5C5.25 6.94772 5.69772 6.5 6.25 6.5H11.25C11.8023 6.5 12.25 6.94772 12.25 7.5V8.5C12.25 9.05228 11.8023 9.5 11.25 9.5H6.25C5.69772 9.5 5.25 9.05228 5.25 8.5V7.5Z" fill="currentColor"/>
+  <path d="M8.25 11C7.69772 11 7.25 11.4477 7.25 12V13C7.25 13.5523 7.69772 14 8.25 14H13.0111C13.5634 14 14.0111 13.5523 14.0111 13V12C14.0111 11.4477 13.5634 11 13.0111 11H8.25Z" fill="currentColor"/>
+  </svg> 
+`,
+};
