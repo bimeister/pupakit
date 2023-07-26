4324f2385c8d46485fe90f384e1d3c4a650ca8eb
Andrei Boreiko <andrei.boreiko@bimeister.com>
2023-07-26 13:12:26 +0500

    feature: BIM-35151 added [autofocus] to pupa-search-field

--- projects/forms/src/components/search-field/search-field.module.ts
+++ projects/forms/src/components/search-field/search-field.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { appCrossCircleIcon, appSearchIcon, PupaIconsModule } from '@bimeister/pupakit.icons';
+import { PupaDirectivesModule } from '@bimeister/pupakit.common';
+import { PupaIconsModule, appCrossCircleIcon, appSearchIcon } from '@bimeister/pupakit.icons';
 import { PupaButtonsModule } from '@bimeister/pupakit.kit';
 import { SearchFieldComponent } from './components/search-field/search-field.component';
 
@@ -13,6 +14,7 @@ import { SearchFieldComponent } from './components/search-field/search-field.com
     ReactiveFormsModule,
     PupaButtonsModule,
     PupaIconsModule.forFeature([appCrossCircleIcon, appSearchIcon]),
+    PupaDirectivesModule,
   ],
   exports: [SearchFieldComponent],
 })
