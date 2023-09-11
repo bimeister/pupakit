e40ac4e9a81288fe588a13b9df668d922faaf416
chilledpaper <denis.mironov@bimeister.com>
2023-09-11 14:37:35 +0300

    fix: BIM-20235 input-number add updateTextValueToNumberViewValue update

--- projects/forms/src/components/input/components/input-number/input-number.component.ts
+++ projects/forms/src/components/input/components/input-number/input-number.component.ts
@@ -3,6 +3,15 @@ import { isNil } from '@bimeister/utilities';
 import { InputBase } from '../../../../declarations/classes/abstract/input-base.abstract';
 import { ValueType } from '../../../../declarations/types/input-value.type';
 
+const DECIMAL_DOT: string = '.';
+const DECIMAL_COMMA: string = ',';
+
+const CHAR_HYPHEN_MINUS: string = '\u002D';
+
+const NEGATIVE_SIGN_REGEX: RegExp = /^\D*[−\-–—]/;
+const NOT_A_NUMBER_AND_DOT_REGEX: RegExp = /[^\d^.]/g;
+const DOT_REGEX: RegExp = /[.]/g;
+
 @Component({
   selector: 'pupa-input-number',
   templateUrl: './input-number.component.html',
@@ -19,5 +28,34 @@ export class InputNumberComponent extends InputBase<ValueType> {
   public reset(): void {
     this.updateValue('');
     this.inputElementRef.nativeElement.focus();
+  }
+
+  public updateTextValueToNumberViewValue(updatedValue: ValueType): void {
+    const value: string = isNil(updatedValue) ? '' : String(updatedValue);
+
+    const hasNegativeSign: boolean = !isNil(value.match(NEGATIVE_SIGN_REGEX));
+
+    const serializedValue: string = this.serializeValue(value);
+
+    const updatedNumberValue: string = (hasNegativeSign ? CHAR_HYPHEN_MINUS : '') + serializedValue;
+
+    this.inputElementRef.nativeElement.value = updatedNumberValue;
+    this.updateValue(serializedValue && serializedValue !== DECIMAL_DOT ? updatedNumberValue : '');
+  }
+
+  private serializeValue(value: string): string {
+    const serializedValue: string = value.replace(DECIMAL_COMMA, DECIMAL_DOT).replace(NOT_A_NUMBER_AND_DOT_REGEX, '');
+
+    const firstDotIndex: number = serializedValue.indexOf(DECIMAL_DOT);
+
+    if (firstDotIndex !== -1) {
+      const integerPartWithFirstDot: string = serializedValue.substring(0, firstDotIndex) + DECIMAL_DOT;
+
+      const decimalPart: string = serializedValue.substring(firstDotIndex + 1).replace(DOT_REGEX, '');
+
+      return integerPartWithFirstDot + decimalPart;
+    }
+
+    return serializedValue;
   }
 }
