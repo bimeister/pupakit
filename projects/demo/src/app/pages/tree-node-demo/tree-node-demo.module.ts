10bdb139b26f2ccf2c4a12b855a0626c9a4f58b6
myaskovsky <yaroslav.myaskovsky@gmail.com>
2023-11-02 15:26:45 +0300

    feature: BIM-39165 revert

--- projects/demo/src/app/pages/tree-node-demo/tree-node-demo.module.ts
+++ projects/demo/src/app/pages/tree-node-demo/tree-node-demo.module.ts
@@ -6,7 +6,6 @@ import { TreeNodeDemoExample1Component } from './examples/example-1/example-1.co
 import { TreeNodeDemoExample2Component } from './examples/example-2/example-2.component';
 import { DemoSharedModule } from '../../shared/shared.module';
 import { TreeNodeDemoExample3Component } from './examples/example-3/example-3.component';
-import { TreeNodeDemoExample4Component } from './examples/example-4/example-4.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +13,6 @@ import { TreeNodeDemoExample4Component } from './examples/example-4/example-4.co
     TreeNodeDemoExample1Component,
     TreeNodeDemoExample2Component,
     TreeNodeDemoExample3Component,
-    TreeNodeDemoExample4Component,
   ],
   imports: [DemoSharedModule, TreeNodeDemoRoutingModule],
 })
