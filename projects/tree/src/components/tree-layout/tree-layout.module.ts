990d96ec950b6a9051ec0e069c47cdec983415f2
myaskovsky <yaroslav.myaskovsky@gmail.com>
2023-11-02 15:26:45 +0300

    feature: BIM-39165 revert

--- projects/tree/src/components/tree-layout/tree-layout.module.ts
+++ projects/tree/src/components/tree-layout/tree-layout.module.ts
@@ -3,20 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { appChevronDownIcon, appChevronRightIcon, appCircleIcon, PupaIconsModule } from '@bimeister/pupakit.icons';
 import { PupaButtonsModule, PupaCheckboxModule, PupaSpinnerModule } from '@bimeister/pupakit.kit';
-import { TreeNodeDescriptionComponent } from './components/tree-node-description/tree-node-description.component';
 import { TreeNodeSkeletonComponent } from './components/tree-node-skeleton/tree-node-skeleton.component';
-import { TreeNodeTitleComponent } from './components/tree-node-title/tree-node-title.component';
 import { TreeNodeComponent } from './components/tree-node/tree-node.component';
 import { TreeNodeActionsDirective } from './directives/tree-node-actions.directive';
 
 @NgModule({
-  declarations: [
-    TreeNodeComponent,
-    TreeNodeActionsDirective,
-    TreeNodeSkeletonComponent,
-    TreeNodeTitleComponent,
-    TreeNodeDescriptionComponent,
-  ],
+  declarations: [TreeNodeComponent, TreeNodeActionsDirective, TreeNodeSkeletonComponent],
   imports: [
     CdkTreeModule,
     CommonModule,
@@ -25,12 +17,6 @@ import { TreeNodeActionsDirective } from './directives/tree-node-actions.directi
     PupaCheckboxModule,
     PupaSpinnerModule,
   ],
-  exports: [
-    TreeNodeActionsDirective,
-    TreeNodeComponent,
-    TreeNodeSkeletonComponent,
-    TreeNodeTitleComponent,
-    TreeNodeDescriptionComponent,
-  ],
+  exports: [TreeNodeActionsDirective, TreeNodeComponent, TreeNodeSkeletonComponent],
 })
 export class PupaTreeLayoutModule {}
