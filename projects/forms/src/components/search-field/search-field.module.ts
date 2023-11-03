3cb5899d7dbb5051f6cf038dcd43820b8a952847
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-11-03 14:54:13 +0300

    feature: add search field action

--- projects/forms/src/components/search-field/search-field.module.ts
+++ projects/forms/src/components/search-field/search-field.module.ts
@@ -2,20 +2,21 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PupaDirectivesModule } from '@bimeister/pupakit.common';
-import { PupaIconsModule, appCrossCircleIcon, appSearchIcon } from '@bimeister/pupakit.icons';
+import { appCloseSquareIcon, appSearchIcon, PupaIconsModule } from '@bimeister/pupakit.icons';
 import { PupaButtonsModule } from '@bimeister/pupakit.kit';
 import { SearchFieldComponent } from './components/search-field/search-field.component';
+import { SearchFieldActionsRightDirective } from './directives/search-field-actions-right.directive';
 
 @NgModule({
-  declarations: [SearchFieldComponent],
+  declarations: [SearchFieldComponent, SearchFieldActionsRightDirective],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     PupaButtonsModule,
-    PupaIconsModule.forFeature([appCrossCircleIcon, appSearchIcon]),
+    PupaIconsModule.forFeature([appCloseSquareIcon, appSearchIcon]),
     PupaDirectivesModule,
   ],
-  exports: [SearchFieldComponent],
+  exports: [SearchFieldComponent, SearchFieldActionsRightDirective],
 })
 export class PupaSearchFieldModule {}
