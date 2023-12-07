233687bbb5df5ff86ea04172b1e2da73190e1d06
Denis Platonov <zlaylink@yandex.ru>
2023-12-07 14:14:42 +0300

    chore: add drawer button icon disabled attribute

--- projects/overlays/src/components/drawer/components/drawer-button-icon/drawer-button-icon.component.ts
+++ projects/overlays/src/components/drawer/components/drawer-button-icon/drawer-button-icon.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input, OnChanges, ViewEncapsulation } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { isNil } from '@bimeister/utilities';
 import { ClientUiStateHandlerService, ComponentChange, ComponentChanges } from '@bimeister/pupakit.common';
 import { ButtonSize } from '@bimeister/pupakit.kit';
+import { isNil } from '@bimeister/utilities';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'pupa-drawer-button-icon',
@@ -16,6 +16,9 @@ export class DrawerButtonIconComponent implements OnChanges {
   @Input() public icon: string;
   public readonly icon$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
+  @Input() public disabled: boolean = false;
+  public readonly disabled$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
   private readonly isMobile$: Observable<boolean> = this.clientUiStateHandlerService.breakpointIsXs$;
 
   public readonly size$: Observable<ButtonSize> = this.isMobile$.pipe(
@@ -26,6 +29,7 @@ export class DrawerButtonIconComponent implements OnChanges {
 
   public ngOnChanges(changes: ComponentChanges<this>): void {
     this.processIconChange(changes?.icon);
+    this.processDisabledChange(changes?.disabled);
   }
 
   private processIconChange(change: ComponentChange<this, string>): void {
@@ -36,5 +40,15 @@ export class DrawerButtonIconComponent implements OnChanges {
     }
 
     this.icon$.next(currentValue);
+  }
+
+  private processDisabledChange(change: ComponentChange<this, boolean | null>): void {
+    const currentValue: boolean | null = change?.currentValue;
+
+    if (isNil(currentValue)) {
+      return;
+    }
+
+    this.disabled$.next(currentValue);
   }
 }
