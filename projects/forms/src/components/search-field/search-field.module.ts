965573ce6e120b01d39b58274477006be63477e2
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-12-21 17:11:52 +0300

    fix: fix search field actions gap

--- projects/forms/src/components/search-field/search-field.module.ts
+++ projects/forms/src/components/search-field/search-field.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PupaDirectivesModule } from '@bimeister/pupakit.common';
+import { PupaCommonModule, PupaDirectivesModule } from '@bimeister/pupakit.common';
 import { appCloseSquareIcon, appSearchIcon, PupaIconsModule } from '@bimeister/pupakit.icons';
 import { PupaButtonsModule } from '@bimeister/pupakit.kit';
 import { SearchFieldComponent } from './components/search-field/search-field.component';
@@ -14,6 +14,7 @@ import { SearchFieldActionsRightDirective } from './directives/search-field-acti
     FormsModule,
     ReactiveFormsModule,
     PupaButtonsModule,
+    PupaCommonModule,
     PupaIconsModule.forFeature([appCloseSquareIcon, appSearchIcon]),
     PupaDirectivesModule,
   ],
