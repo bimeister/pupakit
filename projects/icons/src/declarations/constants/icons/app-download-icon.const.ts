0f366ebfd0e736aa61c3537a6a023c0d1dc0c86a
Daniil Datskevich <daniil.datskevich@bimeister.com>
2024-01-12 17:04:34 +0300

    fix: BIM-35469 delete icon

--- projects/icons/src/declarations/constants/icons/app-download-icon.const.ts
+++
@@ -1,9 +0,0 @@
-import { IconDefinition } from '../../../declarations/interfaces/icon-definition.interface';
-
-export const appDownloadIcon: IconDefinition = {
-  name: 'app-download',
-  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M8 3C8.27614 3 8.5 3.22386 8.5 3.5L8.5 8.79289L10.1464 7.14645C10.3417 6.95118 10.6583 6.95118 10.8536 7.14645C11.0488 7.34171 11.0488 7.65829 10.8536 7.85355L8.35355 10.3536C8.15829 10.5488 7.84171 10.5488 7.64645 10.3536L5.14645 7.85355C4.95118 7.65829 4.95118 7.34171 5.14645 7.14645C5.34171 6.95118 5.65829 6.95118 5.85355 7.14645L7.5 8.79289L7.5 3.5C7.5 3.22386 7.72386 3 8 3ZM2 13C2 12.7239 2.22386 12.5 2.5 12.5H13.5C13.7761 12.5 14 12.7239 14 13C14 13.2761 13.7761 13.5 13.5 13.5H2.5C2.22386 13.5 2 13.2761 2 13Z" fill="currentColor"/>
-</svg>
-  `,
-};
