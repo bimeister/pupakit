6cb8573c3069b9f96d4d10b7b3376812a61de8e9
Dmitry Ivantsov <dmitry.ivantsov@bimeister.com>
2024-01-19 15:49:11 +0300

    refactor: BIM-37349 code review fixes

---
+++ projects/icons/src/declarations/constants/icons/app-menu-milestone-filled-icon.const.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '../../interfaces/icon-definition.interface';
+
+export const appMenuMilestoneFilledIcon: IconDefinition = {
+  name: 'app-menu-milestone-filled',
+  data: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+  <path d="M7.94782 0.999818C7.46876 1.0037 6.99139 1.18882 6.63171 1.55438L1.54302 6.727C0.823666 7.45811 0.833371 8.64896 1.5645 9.36831L6.73716 14.457C7.46827 15.1763 8.65913 15.1666 9.37849 14.4355L14.4672 9.26352C15.186 8.53208 15.1768 7.34092 14.4457 6.62156L9.27304 1.5329C8.90749 1.17322 8.42688 0.995941 7.94782 0.999818Z" fill="currentColor"/>
+  </svg>
+`,
+};
