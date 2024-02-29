192ae993476374fca451e3bcb65c3ce8eac30512
Tatiana Morekhon <tatiana.morekhon@bimeister.com>
2024-02-29 17:48:37 +0300

    feature: BIM-45302 change max length to 100rem

--- projects/demo/src/app/pages/tooltip-demo/tooltip-demo.module.ts
+++ projects/demo/src/app/pages/tooltip-demo/tooltip-demo.module.ts
@@ -5,6 +5,7 @@ import { TooltipExample2Component } from './examples/example-2/example-2.compone
 import { TooltipExample3Component } from './examples/example-3/example-3.component';
 import { TooltipExample4Component } from './examples/example-4/example-4.component';
 import { TooltipExample5Component } from './examples/example-5/example-5.component';
+import { TooltipExample6Component } from './examples/example-6/example-6.component';
 import { TooltipDemoRoutingModule } from './tooltip-demo-routing.module';
 import { TooltipDemoComponent } from './tooltip-demo.component';
 
@@ -14,6 +15,7 @@ const EXAMPLES: Type<unknown>[] = [
   TooltipExample3Component,
   TooltipExample4Component,
   TooltipExample5Component,
+  TooltipExample6Component,
 ];
 const COMPONENTS: Type<unknown>[] = [TooltipDemoComponent, ...EXAMPLES];
 
