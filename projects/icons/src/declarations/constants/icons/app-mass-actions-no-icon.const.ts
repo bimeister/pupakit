8f5c6ed96a214b08aa6a5006284e76972b0cb0ec
Roman Kuklin <roman.kuklin@bimeister.com>
2024-03-08 18:09:08 +0300

    feature/BIM-44554 added icons: * app-mass-actions-no * app-mass-actions-check * app-unassing-role

---
+++ projects/icons/src/declarations/constants/icons/app-mass-actions-no-icon.const.ts
@@ -0,0 +1,11 @@
+import { IconDefinition } from '../../../declarations/interfaces/icon-definition.interface';
+
+export const appMassActionsNoIcon: IconDefinition = {
+  name: 'app-mass-actions-no',
+  data: `
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M4.83333 1C3.82674 1 3 1.82674 3 2.83333V11.1667C3 12.1733 3.82674 13 4.83333 13H13.1667C14.1733 13 15 12.1733 15 11.1667V2.83333C15 1.82674 14.1733 1 13.1667 1H4.83333ZM4.83333 2H13.1667C13.6327 2 14 2.36726 14 2.83333V11.1667C14 11.6327 13.6327 12 13.1667 12H4.83333C4.36726 12 4 11.6327 4 11.1667V2.83333C4 2.36726 4.36726 2 4.83333 2Z" fill="currentColor"/>
+    <path d="M1 5.49994C1 4.68203 1.39278 3.95586 2 3.49976V12.4999C2 13.3284 2.67157 13.9999 3.5 13.9999H3.98832L4 14H12.5002C12.0441 14.6072 11.3179 14.9999 10.5 14.9999H3.5C2.11929 14.9999 1 13.8807 1 12.4999V5.49994Z" fill="currentColor"/>
+  </svg>
+  `,
+};
