da2841de88494bd042fc27e5c15387e2eb86b5e6
Roman Kuklin <roman.kuklin@bimeister.com>
2024-06-20 13:19:51 +0300

    feature/BIM-51275 added icons: * app-square-measure * app-square-dot

---
+++ projects/icons/src/declarations/constants/icons/app-square-dot-icon.const.ts
@@ -0,0 +1,10 @@
+import { IconDefinition } from '../../interfaces/icon-definition.interface';
+
+export const appSquareDotIcon: IconDefinition = {
+  name: 'app-square-dot',
+  data: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6 8C6 6.89543 6.89543 6 8 6C9.10457 6 10 6.89543 10 8C10 9.10457 9.10457 10 8 10C6.89543 10 6 9.10457 6 8Z" fill="currentColor"/>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M2 3.83333C2 2.82674 2.82674 2 3.83333 2H12.1667C13.1733 2 14 2.82674 14 3.83333V12.1667C14 13.1733 13.1733 14 12.1667 14H3.83333C2.82674 14 2 13.1733 2 12.1667V3.83333ZM12.1667 3H3.83333C3.36726 3 3 3.36726 3 3.83333V12.1667C3 12.6327 3.36726 13 3.83333 13H12.1667C12.6327 13 13 12.6327 13 12.1667V3.83333C13 3.36726 12.6327 3 12.1667 3Z" fill="currentColor"/>
+</svg>
+`,
+};
