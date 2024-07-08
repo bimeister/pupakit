7617aeedfad2b01e0c7b81176c232c538c86c024
Vladislav Babutskiy <vbabutskiy@gmail.com>
2024-07-08 09:22:04 +0300

    fix: BIM-52228 button styles and status-square icon

--- projects/icons/src/declarations/constants/icons/app-status-square-icon.const.ts
+++ projects/icons/src/declarations/constants/icons/app-status-square-icon.const.ts
@@ -3,7 +3,7 @@ import { IconDefinition } from '../../../declarations/interfaces/icon-definition
 export const appStatusSquareIcon: IconDefinition = {
   name: 'app-status-square',
   data: `<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M3.4353 4.72845C3.4353 4.17617 3.88302 3.72845 4.4353 3.72845H8.4353C8.98759 3.72845 9.4353 4.17617 9.4353 4.72845V8.72845C9.4353 9.28074 8.98759 9.72845 8.4353 9.72845H4.4353C3.88302 9.72845 3.4353 9.28074 3.4353 8.72845V4.72845Z" fill="currentColor"/>
+<path d="M3 4C3 3.44772 3.44772 3 4 3H8C8.55228 3 9 3.44772 9 4V8C9 8.55228 8.55228 9 8 9H4C3.44772 9 3 8.55228 3 8V4Z" fill="currentColor"/>
   </svg>
 `,
 };
